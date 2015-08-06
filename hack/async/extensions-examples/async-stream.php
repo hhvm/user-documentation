296c3120fece5aeb82cdc1794f296a757262b48f
JoelMarcey <joelm@fb.com>
2015-08-06 12:58:09 -0700

    Add missing stream example

---
+++ hack/async/extensions-examples/async-stream.php
@@ -0,0 +1,21 @@
+<?hh
+
+function get_resources(): array<resource> {
+  $r1 = fopen('php://stdout', 'w');
+  $r2 = fopen('php://stdout', 'w');
+  $r3 = fopen('php://stdout', 'w');
+
+  return array($r1, $r2, $r3);
+}
+
+async function write_all(array<resource> $resources): Awaitable<void> {
+  $write_single_resource = async function(resource $r) {
+    $status = await stream_await($r, STREAM_AWAIT_WRITE, 1.0);
+    if ($status === STREAM_AWAIT_READY) {
+      fwrite($r, time() . PHP_EOL);
+    }
+  };
+  await HH\Asio\v(array_map($write_single_resource, $resources));
+}
+
+HH\Asio\join(write_all(get_resources()));
