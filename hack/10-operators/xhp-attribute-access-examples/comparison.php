a86f8b339eb95371036d79b1afdfc8fc04f7631b
JoelMarcey <joelm@fb.com>
2015-08-18 10:43:02 -0700

    Add documentation for XHP attribute access operator

---
+++ hack/10-operators/xhp-attribute-access-examples/comparison.php
@@ -0,0 +1,18 @@
+<?hh
+
+// For XHP library, etc.
+require __DIR__ . "/../../../vendor/autoload.php";
+
+class :comparison:greeting extends :x:element {
+  attribute string first = "Good";
+  attribute string last = "Afternoon";
+  public function render(): XHPRoot {
+    // Using getattribute()
+    $ret = $this->getAttribute("first");
+    // using ->:
+    $ret .= $this->:last;
+    return <x:frag>{$ret}</x:frag>;
+  }
+}
+
+echo <comparison:greeting />;
