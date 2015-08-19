2dd14eae961d81e8f4f34270b8f7e90cbb4dc722
Fred Emmott <fe@fb.com>
2015-08-19 14:07:26 -0700

    Correctly merge generic interfaces
    HH\KeyedIterable now shows up as extending both Iterator<Tv> and HH\Iterable<Tv>
    
    Fixes #2

--- src/MergedDataBuilder.php
+++ src/MergedDataBuilder.php
@@ -14,6 +14,24 @@ final class MergedDataBuilder {
     $this->mergeField($new, 'generics');
     $this->mergeField($new, 'methods');
     $this->mergeField($new, 'docComment');
+
+    $p1 = idx($this->data, 'parent');
+    $p2 = idx($new, 'parent');
+    if ($p1 === null || $p2 === null) {
+      $parent = ($p1 !== null) ? $p1 : $p2;
+    } else {
+      // UNSAFE mixed to shape coercion
+      $parent = self::MergedTypehint($p1, $p2);
+    }
+    if ($parent !== null) {
+      $this->data['parent'] = $parent;
+    }
+
+    $interfaces = new Vector(/*UNSAFE_EXPR*/idx($this->data, 'interfaces', []));
+    $interfaces->addAll(/*UNSAFE_EXPR*/idx($new, 'interfaces', []));
+    if ($interfaces) {
+      $this->data['interfaces'] = self::MergedInterfaces($interfaces);
+    }
     return $this;
   }
 
@@ -30,5 +48,52 @@ final class MergedDataBuilder {
     if ($value !== null && $value != []) {
       $this->data[$key] = $value;
     }
+  }
+
+  private static function MergedTypehint(
+    ?TypehintDocumentation $a,
+    ?TypehintDocumentation $b,
+  ): ?TypehintDocumentation {
+    if ($a === null) {
+      return $b;
+    }
+    if ($b === null) {
+      return $a;
+    }
+
+    $name = $a['typename'];
+    if (strpos($name, "\\") === false) {
+      $name = $b['typename'];
+    }
+    $generics = $a['genericTypes'];
+    if (count($generics) === 0) {
+      $generics = $b['genericTypes'];
+    }
+
+    return shape(
+      'typename' => $name,
+      'genericTypes' => $generics,
+    );
+  }
+
+  private static function MergedInterfaces(
+    \ConstVector<TypehintDocumentation> $interfaces,
+  ): array<TypehintDocumentation> {
+    $keyed = Map { };
+    foreach ($interfaces as $interface) {
+      $name = $interface['typename'];
+      $ns_sep = strrpos($name, "\\");
+      if ($ns_sep === false) {
+        $key = $name;
+      } else {
+        $key = substr($name, $ns_sep + 1);
+      }
+      if ($keyed->containsKey($key)) {
+        $keyed[$key] = self::MergedTypehint($keyed[$key], $interface);
+      } else {
+        $keyed[$key] = $interface;
+      }
+    }
+    return array_values($keyed->toArray());
   }
 }
--- src/MergedYAMLBuilder.php
+++ src/MergedYAMLBuilder.php
@@ -23,57 +23,10 @@ final class MergedYAMLBuilder {
   }
 
   public function build(): void {
-    $this->normalizeAll();
-
     $writer = new YAMLWriter($this->destination);
     foreach ($this->definitions as $def) {
       $writer->write($def);
     }
-  }
-
-  private function normalizeAll(): void {
-    static $called = false;
-    invariant(!$called, 'normalizeAll called twice');
-    $called = true;
-
-    foreach ($this->definitions as $key => $def) {
-      $parent = $this->getParent($def);
-      if ($parent !== null) {
-        $def['parent'] = $this->normalizeType($parent);
-      }
-      $interfaces = $this->getInterfaces($def);
-      if ($interfaces !== null) {
-        $def['interfaces'] = array_map(
-          $interface ==> $this->normalizeType($interface),
-          $interfaces,
-        );
-      }
-      $this->definitions[$key] = $def;
-    }
-  }
-
-  private function normalizeType(
-    TypehintDocumentation $type,
-  ): TypehintDocumentation {
-    $name = $type['typename'];
-    $key = self::GetMergeKey('class', $name);
-    $matches = $this->definitions->containsKey($key);
-    if (!$matches) {
-      $matches = preg_match('^/T[a-z0-9][A-Za-z0-9]*$/', $name);
-    }
-    invariant(
-      $matches,
-      'could not find definition for %s',
-      $key,
-    );
-    $canonical = $this->definitions->at($key);
-    return shape(
-      'typename' => $canonical['data']['name'],
-      'genericTypes' => array_map(
-        $t ==> /* UNSAFE_EXPR */ $this->normalizeType($t),
-        $type['genericTypes'],
-      ),
-    );
   }
 
   public function addDefinition(BaseYAML $def): this {
@@ -100,15 +53,5 @@ final class MergedYAMLBuilder {
      // UNSAFE array to shape conversion
     $this->definitions[$key] = $merged;
     return $this;
-  }
-
-  private function getParent(BaseYAML $def): ?TypehintDocumentation {
-    // UNSAFE
-    return idx(Shapes::toArray($def), 'parent');
-  }
-
-  private function getInterfaces(BaseYAML $def): ?array<TypehintDocumentation> {
-    // UNSAFE
-    return idx(Shapes::toArray($def), 'interfaces');
   }
 }
