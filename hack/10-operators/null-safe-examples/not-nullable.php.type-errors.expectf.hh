6f10062544389c8bbff9d73cad5d2888d633fa52
JoelMarcey <joelm@fb.com>
2015-08-27 20:15:15 -0700

    Make operator examples test runnable

---
+++ hack/10-operators/null-safe-examples/not-nullable.php.type-errors.expectf.hh
@@ -0,0 +1,2 @@
+%snot-nullable.php:19:10,19: You are using the ?-> operator but this object cannot be null.  (Typing[4117])
+  %snot-nullable.php:11:21,23: This is what makes me believe it cannot be null
