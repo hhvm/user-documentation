c9ba03252ad16c1add489c89888173da8ed475b4
Fred Emmott <fe@fb.com>
2015-09-10 10:17:39 -0700

    Update markdown renderer to support new layout

--- md-render/HHVM/UserDocumentation/IncludeExamplesFilter.rb
+++ md-render/HHVM/UserDocumentation/IncludeExamplesFilter.rb
@@ -4,12 +4,24 @@ module HHVM
   module UserDocumentation
     class IncludeExamplesFilter < HTML::Pipeline::Filter
       def call
-        base = File.dirname(context[:file])
+        file = context[:file]
+
+        # Actual directory is something like 01-foo-examples/ ...
+        examples_dir = File.basename(file, '.md') + '-examples'
+        # ... but to make reordering easier, the references to it in the
+        # markdown skip the '01-' prefix.
+        match = /^[0-9]+-(?<source_dir>.+)/.match(examples_dir)
+        return(doc) if match.nil?
+        source_examples_dir = match['source_dir']
+
+        base = File.expand_path(examples_dir, File.dirname(file))
+
         doc.search('p').each do |node|
           contents = node.inner_text.strip
-          if match_data = /^@@ (?<relative_path>[^ ]+\.php(?:.type-errors)?) @@$/.match(contents)
-            relative_path = match_data['relative_path']
-            full_path = File.expand_path(relative_path, base)
+          if match = %r,^@@ (?<dir_name>[^/ ]+)/(?<file_name>[^/ ]+\.php(?:.type-errors)?) @@$,.match(contents)
+            next if match[:dir_name] != source_examples_dir
+
+            full_path = File.expand_path(match[:file_name], base)
 
             if ! File.exists? full_path
               text = "Missing example: #{full_path}"
