713bbf3a6487c4f10853bcdceb2f1030e3617216
JoelMarcey <joelm@fb.com>
2015-09-16 16:56:41 -0700

    Reorder mysqli_connection parameters so the environment check actually works

--- guides/hack/99-api-examples/async-mysql-examples/require_env.inc.php
+++ guides/hack/99-api-examples/async-mysql-examples/require_env.inc.php
@@ -9,7 +9,7 @@ use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
 if (!extension_loaded('mysql') || !function_exists('mysqli_connect')) {
   die('Skip');
 }
-if (!mysqli_connect(CI::$host, CI::$port, CI::$user, CI::$passwd, CI::$db)) {
+if (!mysqli_connect(CI::$host, CI::$user, CI::$passwd, CI::$db, CI::$port)) {
   die('Skip');
 }
 
