68466a2842555de7344715a45530f3c47cd7ed54
JoelMarcey <joelm@fb.com>
2015-09-17 12:48:27 -0700

    Add `AsyncMysqlConnection::query` example

---
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-connection-examples/query.md
@@ -0,0 +1,1 @@
+The following example shows a basic usage of `AsyncMysqlConneciton::query`. First you get a connection from an `AsyncMysqlConnectionPool`, then you can make the query.
---
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-connection-examples/query.php
@@ -0,0 +1,31 @@
+<?hh
+
+namespace Hack\UserDocumentation\API\Examples\AsyncMysql\Conn\Query;
+
+require __DIR__ .'/../connect.inc.php';
+
+use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
+
+async function connect(\AsyncMysqlConnectionPool $pool):
+  Awaitable<\AsyncMysqlConnection> {
+  return await $pool->connect(
+    CI::$host,
+    CI::$port,
+    CI::$db,
+    CI::$user,
+    CI::$passwd
+  );
+}
+async function simple_query(): Awaitable<int> {
+  $pool = new \AsyncMysqlConnectionPool(array());
+  $conn = await connect($pool);
+  $result = await $conn->query('SELECT name FROM test_table WHERE userID = 1');
+  return $result->numRows();
+}
+
+function run(): void {
+  $r = \HH\Asio\join(simple_query());
+  var_dump($r);
+}
+
+run();
