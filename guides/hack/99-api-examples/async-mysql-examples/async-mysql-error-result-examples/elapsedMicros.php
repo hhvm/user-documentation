5fca52f91d27daf294e6399aa6bacd2763cf4b26
JoelMarcey <joelm@fb.com>
2015-09-21 15:28:23 -0700

    Add AsyncMysqlErrorResult::elapsedMicros example

--- guides/hack/99-api-examples/async-mysql-examples/async-mysql-error-result-examples/elapsedMicros.php
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-error-result-examples/elapsedMicros.php
@@ -1,7 +1,41 @@
 <?hh
 
-namespace Hack\UserDocumentation\APIxamples\ZZZZZ\YYYYY\XXXXX;
+namespace Hack\UserDocumentation\API\Examples\AsyncMysql\ErrorResult\ElapsedMic;
 
 require __DIR__ .'/../connect.inc.php';
 
-use \Hack\UserDocumentation\APIxamples\AsyncMysql\ConnectionInfo as CI;
+use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
+
+async function connect(\AsyncMysqlConnectionPool $pool):
+  Awaitable<\AsyncMysqlConnection> {
+  return await $pool->connect(
+    CI::$host,
+    CI::$port,
+    CI::$db,
+    CI::$user,
+    CI::$passwd
+  );
+}
+async function simple_query_error(): Awaitable<int> {
+  $pool = new \AsyncMysqlConnectionPool(array());
+  $conn = await connect($pool);
+  try {
+    $result = await $conn->query('SELECT bogus FROM bogus WHERE bogus = 1');
+  } catch (\AsyncMysqlQueryException $ex) {
+    $qr = $ex->getResult();
+    // Actually `AsyncMysqlQueryErrorResult`
+    var_dump($qr instanceof \AsyncMysqlErrorResult);
+    var_dump($qr->elapsedMicros());
+    $conn->close();
+    return 0;
+  }
+  $conn->close();
+  return $result->numRows();
+}
+
+function run(): void {
+  $r = \HH\Asio\join(simple_query_error());
+  var_dump($r);
+}
+
+run();
