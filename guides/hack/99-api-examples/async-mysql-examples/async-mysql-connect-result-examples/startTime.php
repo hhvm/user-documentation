4682dfd78c291522aecc3ccff8562d09cf21d08f
JoelMarcey <joelm@fb.com>
2015-09-21 14:04:18 -0700

    Add AsyncMysqlConnectResult::startTime example

--- guides/hack/99-api-examples/async-mysql-examples/async-mysql-connect-result-examples/startTime.php
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-connect-result-examples/startTime.php
@@ -1,7 +1,32 @@
 <?hh
 
-namespace Hack\UserDocumentation\APIxamples\ZZZZZ\YYYYY\XXXXX;
+namespace Hack\UserDocumentation\API\Examples\AsyncMysql\ConnResult\ElapsedMic;
 
 require __DIR__ .'/../connect.inc.php';
 
-use \Hack\UserDocumentation\APIxamples\AsyncMysql\ConnectionInfo as CI;
+use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
+
+async function connect(\AsyncMysqlConnectionPool $pool):
+  Awaitable<\AsyncMysqlConnection> {
+  return await $pool->connect(
+    CI::$host,
+    CI::$port,
+    CI::$db,
+    CI::$user,
+    CI::$passwd
+  );
+}
+async function get_connection_start_time(): Awaitable<float> {
+  $pool = new \AsyncMysqlConnectionPool(array());
+  $conn = await connect($pool);
+  $st = $conn->connectResult()->endTime();
+  $conn->close();
+  return $st;
+}
+
+function run(): void {
+  $st = \HH\Asio\join(get_connection_start_time());
+  var_dump($st);
+}
+
+run();
