f7f036b8ad6dc9af6c9646f217d312befb8628f5
JoelMarcey <joelm@fb.com>
2015-09-21 12:57:36 -0700

    Add stubs for the rest of the Async MySql examples

---
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-query-exception-examples/__construct.php
@@ -0,0 +1,7 @@
+<?hh
+
+namespace Hack\UserDocumentation\APIxamples\ZZZZZ\YYYYY\XXXXX;
+
+require __DIR__ .'/../connect.inc.php';
+
+use \Hack\UserDocumentation\APIxamples\AsyncMysql\ConnectionInfo as CI;
