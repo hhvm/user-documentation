a54da496839e9cc8f91569596d0456900a8e98a3
JoelMarcey <joelm@fb.com>
2015-09-21 16:53:16 -0700

    Add AsyncMysqlQueryResult::elapsedMicros example

--- guides/hack/99-api-examples/async-mysql-examples/async-mysql-query-result-examples/elapsedMicros.php
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-query-result-examples/elapsedMicros.php
@@ -1,7 +1,34 @@
 <?hh
 
-namespace Hack\UserDocumentation\APIxamples\ZZZZZ\YYYYY\XXXXX;
+namespace Hack\UserDocumentation\API\Examples\AsyncMysql\QueryResult\ElapsedMic;
 
 require __DIR__ .'/../connect.inc.php';
 
-use \Hack\UserDocumentation\APIxamples\AsyncMysql\ConnectionInfo as CI;
+use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
+
+async function connect(\AsyncMysqlConnectionPool $pool):
+  Awaitable<\AsyncMysqlConnection> {
+  return await $pool->connect(
+    CI::$host,
+    CI::$port,
+    CI::$db,
+    CI::$user,
+    CI::$passwd
+  );
+}
+async function simple_query(): Awaitable<int> {
+  $pool = new \AsyncMysqlConnectionPool(array());
+  $conn = await connect($pool);
+  $result = await $conn->query('SELECT name FROM test_table WHERE userID = 1');
+  // How long did it take to get this result?
+  var_dump($result->elapsedMicros());
+  $conn->close();
+  return $result->numRows();
+}
+
+function run(): void {
+  $r = \HH\Asio\join(simple_query());
+  var_dump($r);
+}
+
+run();
