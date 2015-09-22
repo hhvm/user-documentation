1cb246bad81fa79172d8384f032f5a723f768f20
JoelMarcey <joelm@fb.com>
2015-09-22 08:08:42 -0700

    Add AsyncMysqlRowBlock::getFieldAsDouble example

--- guides/hack/99-api-examples/async-mysql-examples/async-mysql-row-block-examples/getFieldAsDouble.php
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-row-block-examples/getFieldAsDouble.php
@@ -1,7 +1,43 @@
 <?hh
 
-namespace Hack\UserDocumentation\APIxamples\ZZZZZ\YYYYY\XXXXX;
+namespace Hack\UserDocumentation\API\Examples\AsyncMysql\RowBlock\FieldsCount;
 
 require __DIR__ .'/../connect.inc.php';
 
-use \Hack\UserDocumentation\APIxamples\AsyncMysql\ConnectionInfo as CI;
+use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
+
+async function connect(\AsyncMysqlConnectionPool $pool):
+  Awaitable<\AsyncMysqlConnection> {
+  return await $pool->connect(
+    CI::$host,
+    CI::$port,
+    CI::$db,
+    CI::$user,
+    CI::$passwd
+  );
+}
+async function simple_query(): Awaitable<float> {
+  $pool = new \AsyncMysqlConnectionPool(array());
+  $conn = await connect($pool);
+  $result = await $conn->query(
+    'SELECT * FROM test_table WHERE userID < 50'
+  );
+  $conn->close();
+  // A call to $result->rowBlocks() actually pops the first element of the
+  // row block Vector. So the call actually mutates the Vector.
+  $row_blocks = $result->rowBlocks();
+  if ($row_blocks->count() > 0) {
+    // An AsyncMysqlRowBlock
+    $row_block = $row_blocks[0];
+    return $row_block->getFieldAsDouble(0, 'age'); // int will be a float value
+  } else {
+    return -1;
+  }
+}
+
+function run(): void {
+  $r = \HH\Asio\join(simple_query());
+  var_dump($r);
+}
+
+run();
