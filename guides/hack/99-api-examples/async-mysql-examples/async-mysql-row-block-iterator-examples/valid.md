b25ced47b89e441a8254ca0c4e7236867089728a
JoelMarcey <joelm@fb.com>
2015-09-22 13:16:54 -0700

    Add AsyncMysqlRowBlockIterator::valid example

---
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-row-block-iterator-examples/valid.md
@@ -0,0 +1,1 @@
+The following example shows how to use `AsyncMysqlRowBlockIterator::valid` to determine whether the current iterator is still valid (i.e., there was actually something to iterate over, or we have reached the end)
--- guides/hack/99-api-examples/async-mysql-examples/async-mysql-row-block-iterator-examples/valid.php
+++ guides/hack/99-api-examples/async-mysql-examples/async-mysql-row-block-iterator-examples/valid.php
@@ -1,7 +1,61 @@
 <?hh
 
-namespace Hack\UserDocumentation\APIxamples\ZZZZZ\YYYYY\XXXXX;
+namespace Hack\UserDocumentation\API\Examples\AsyncMysql\RowBlockIt\Valid;
 
 require __DIR__ .'/../connect.inc.php';
 
-use \Hack\UserDocumentation\APIxamples\AsyncMysql\ConnectionInfo as CI;
+use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
+
+async function connect(\AsyncMysqlConnectionPool $pool):
+  Awaitable<\AsyncMysqlConnection> {
+  return await $pool->connect(
+    CI::$host,
+    CI::$port,
+    CI::$db,
+    CI::$user,
+    CI::$passwd
+  );
+}
+async function iterate(): Awaitable<int> {
+  $pool = new \AsyncMysqlConnectionPool(array());
+  $conn = await connect($pool);
+  $result = await $conn->query(
+    'SELECT * FROM test_table WHERE userID < 50'
+  );
+  $conn->close();
+  // A call to $result->rowBlocks() actually pops the first element of the
+  // row block Vector. So the call actually mutates the Vector.
+  $row_blocks = $result->rowBlocks();
+  if ($row_blocks->count() > 0) {
+    // An AsyncMysqlRowBlock
+    $row_block = $row_blocks[0];
+    // An AsyncMysqlRowBlockIterator
+    $rbit = $row_block->getIterator();
+    // Iterating through a row block iterator will have an int key and
+    // an AsyncMysqlRow as its value
+    while ($rbit->valid()) {
+      // current() gets you an AsyncMysqlRow
+      $row = $rbit->current();
+      // getIterator() gets you an AsyncmysqlRowIterator
+      $rit = $row->getIterator();
+      while ($rit->valid()) {
+        // current() will give you a string value of the field in the row
+        if ($rit->key() > 0 && is_numeric($rit->current())) {
+          return intval($rit->current());
+        }
+        $rit->next();
+      }
+      $rbit->next();
+    }
+    return -1;
+  } else {
+    return -1;
+  }
+}
+
+function run(): void {
+  $r = \HH\Asio\join(iterate());
+  var_dump($r);
+}
+
+run();
