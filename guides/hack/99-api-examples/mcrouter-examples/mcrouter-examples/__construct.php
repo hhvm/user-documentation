972e8a34b9716b28a06d055d737b33dd4b5a8ae9
JoelMarcey <joelm@fb.com>
2015-09-23 13:00:50 -0700

    Add MCrouter::__construct example

--- guides/hack/99-api-examples/mcrouter-examples/mcrouter-examples/__construct.php
+++ guides/hack/99-api-examples/mcrouter-examples/mcrouter-examples/__construct.php
@@ -1,3 +1,26 @@
 <?hh
 
 namespace Hack\UserDocumentation\API\Examples\MCRouter\MCrouter\Construct;
+
+function construct_mcrouter(): void {
+  $servers = Vector { getenv('HHVM_TEST_MCROUTER') };
+  // For many use cases, calling MCRouter::createSimple($servers) would
+  // suffice here. But this shows you how to explicitly create the configuration
+  // options for creating an instance of MCRouter
+  $options = array(
+    'config_str' => json_encode(
+      array(
+        'pools' => array(
+          'P' => array(
+            'servers' => $servers,
+          ),
+        ),
+        'route' => 'PoolRoute|P',
+      ),
+    ),
+  );
+  $mc = new \MCRouter($options); // could also pass a persistence id string here
+  var_dump($mc instanceof \MCRouter);
+}
+
+construct_mcrouter();
