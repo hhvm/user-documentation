368e1d182eb089fcaa974721e44da401be4bedc2
JoelMarcey <joelm@fb.com>
2015-09-23 13:44:10 -0700

    Add MCRouter::set example

--- guides/hack/99-api-examples/mcrouter-examples/mcrouter-examples/add.md
+++ guides/hack/99-api-examples/mcrouter-examples/mcrouter-examples/add.md
@@ -1,1 +1,1 @@
-The following example shows how to use `MCRouter::add` to add a unique key/value pair to the memcached server. You cannot `add` the same key twice; if you need to upate a key, use `MCRouter::set`
+The following example shows how to use `MCRouter::add` to add a unique key/value pair to the memcached server. You cannot `add` the same key twice; if you need to update a key, use `MCRouter::set`
--- guides/hack/99-api-examples/mcrouter-examples/mcrouter-examples/set.php
+++ guides/hack/99-api-examples/mcrouter-examples/mcrouter-examples/set.php
@@ -1,3 +1,32 @@
 <?hh
 
 namespace Hack\UserDocumentation\API\Examples\MCRouter\MCrouter\Set;
+
+function get_simple_mcrouter(): \MCRouter {
+  $servers = Vector { getenv('HHVM_TEST_MCROUTER') };
+  $mc = \MCRouter::createSimple($servers);
+  return $mc;
+}
+
+async function set_value(\MCRouter $mc, string $key,
+                         string $value): Awaitable<void> {
+  await $mc->set($key, $value); // optional flags and expiration time
+}
+
+async function run(): Awaitable<void> {
+  $mc = get_simple_mcrouter();
+  $unique_key = str_shuffle('ABCDEFGHIJKLMN');
+  await set_value($mc, $unique_key, "Hi");
+  $val = await $mc->get($unique_key);
+  var_dump($val);
+  try {
+    // Setting the same key again is fine
+    await set_value($mc, $unique_key, "Bye");
+    $val = await $mc->get($unique_key);
+    var_dump($val);
+  } catch (\MCRouterException $ex) {
+    var_dump($ex->getMessage()); // We should not get here
+  }
+}
+
+\HH\Asio\join(run());
