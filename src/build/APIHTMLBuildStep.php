hhvm/user-documentation

commit 6f6fb00b634402ae552491db0e92ee4c390839aa

Make the renderer non-blocking
Executing the render program asynchronously reduces the build time on my
machine from 8 minutes to 4.

src/build/APIHTMLBuildStep.php
@@ -45,7 +45,7 @@ final class APIHTMLBuildStep extends BuildStep {
     }
 
     $input = realpath(self::SOURCE_ROOT.'/'.$input);
-    shell_exec(sprintf("%s %s > %s", self::RENDERER, $input, $output));
+    shell_exec(sprintf("%s %s > %s &", self::RENDERER, $input, $output));
     return $output;
   }
 

src/build/GuidesHTMLBuildStep.php
@@ -55,7 +55,7 @@ final class GuidesHTMLBuildStep extends BuildStep {
     $input = realpath(self::SOURCE_ROOT.'/'.$input);
     $args = (Vector { self::RENDERER, $input, $output })
       ->map($raw ==> escapeshellarg($raw));
-    shell_exec(sprintf("%s %s > %s", ...$args));
+    shell_exec(sprintf("%s %s > %s &", ...$args));
     $this->addBookmarksToHeadings($output);
     $this->fixInternalLinks($output);
     return $output;

commit 433f5c60f194a7d6dd4792208648724772c39203

Wait for render processes to finish
This aims to fix a problem I introduced in 6f6fb00b634402.

APIHTMLBuildStep now still opens up as many processes as the CPU can handle
but waits for them to finish before we proceed to create the index.

GuidesHTMLBuildStep on the other hand do some processing with rendered files
right after rendering them. So keep that build step synchronous.
APIHTMLBuildStep is the one that takes most of the time anyway.

src/build/APIHTMLBuildStep.php
@@ -7,6 +7,8 @@ final class APIHTMLBuildStep extends BuildStep {
   const string RENDERER = __DIR__.'/../../md-render/render.rb';
   const string METHOD_DELIM = "method";
 
+  private \Vector<resource> $renderprocs = Vector {};
+
   public function buildAll(): void {
     Log::i("\nAPIHTMLBuild");
     $sources = self::findSources(self::SOURCE_ROOT, Set{'md'})
@@ -15,11 +17,19 @@ final class APIHTMLBuildStep extends BuildStep {
       ->map($path ==> substr($path, strlen(self::SOURCE_ROOT) + 1));
     sort($sources);
 
+    Log::i("\nSpawning render processes");
     $list = Vector { };
     foreach ($sources as $input) {
       Log::v('.');
       $output = $this->renderFile($input);
       $list[] = $output;
+    }
+
+    Log::i("\nWait for render processes to finish");
+    $this->renderprocs = $this->renderprocs->filter($p ==> proc_close($p) != 0);
+    if ($this->renderprocs->count() > 0) {
+      Log::w(sprintf("\n%d render processes exited with an error",
+                $this->renderprocs->count()));
     }
 
     $index = $this->createIndex($list);
@@ -45,7 +55,10 @@ final class APIHTMLBuildStep extends BuildStep {
     }
 
     $input = realpath(self::SOURCE_ROOT.'/'.$input);
-    shell_exec(sprintf("%s %s > %s &", self::RENDERER, $input, $output));
+    $unused = null;
+    $this->renderprocs[] =
+      proc_open(sprintf("%s %s > %s", self::RENDERER, $input, $output),
+                [], $unused);
     return $output;
   }
 

src/build/GuidesHTMLBuildStep.php
@@ -55,7 +55,7 @@ final class GuidesHTMLBuildStep extends BuildStep {
     $input = realpath(self::SOURCE_ROOT.'/'.$input);
     $args = (Vector { self::RENDERER, $input, $output })
       ->map($raw ==> escapeshellarg($raw));
-    shell_exec(sprintf("%s %s > %s &", ...$args));
+    shell_exec(sprintf("%s %s > %s", ...$args));
     $this->addBookmarksToHeadings($output);
     $this->fixInternalLinks($output);
     return $output;