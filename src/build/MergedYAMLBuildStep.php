794c41eda1dc548ecdb47d984e8da346cf0e32dd
JoelMarcey <joelm@fb.com>
2015-10-27 16:42:07 -0700

    We should be merging and overriding into HHI from HNI/Systemlib, not the other way around
    Eventually all HHI info will be in HNI. HHI is just a placeholder until everything (like
    collections) is converted into HNI.
    
    This way we get more documentation out of the deal since HNI is more documented than HHI
    in general.

--- src/build/MergedYAMLBuildStep.php
+++ src/build/MergedYAMLBuildStep.php
@@ -6,13 +6,16 @@ final class MergedYAMLBuildStep extends BuildStep {
   public function buildAll(): void {
     Log::i("\nMergedYAMLBuild");
     $sources = (Vector { })
-      ->addAll(self::findSources(BuildPaths::SYSTEMLIB_YAML, Set{'yml'}))
-      ->addAll(self::findSources(BuildPaths::HHI_YAML, Set{'yml'}));
+      ->addAll(self::findSources(BuildPaths::HHI_YAML, Set{'yml'}))
+      ->addAll(self::findSources(BuildPaths::SYSTEMLIB_YAML, Set{'yml'}));
     if (!is_dir(BuildPaths::MERGED_YAML)) {
       mkdir(BuildPaths::MERGED_YAML, /* mode = */ 0755, /* recursive = */ true);
     }
     $builder = new MergedYAMLBuilder(BuildPaths::MERGED_YAML);
     foreach ($sources as $source) {
+      if (strpos($source, 'MCRouter') !== false) {
+        $x = 2;
+      }
       Log::v('.');
       $builder->addDefinition(\Spyc::YAMLLoad($source));
     }
