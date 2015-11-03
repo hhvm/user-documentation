5c812d4a21fd3085600f4901cda926694f210800
JoelMarcey <joelm@fb.com>
2015-11-03 13:14:25 -0800

    We actually do want all the build steps
    :)

--- bin/build.php
+++ bin/build.php
@@ -3,7 +3,12 @@ namespace HHVM\UserDocumentation;
 
 require(__DIR__.'/../vendor/autoload.php');
 function hhvm_to_yaml(): void {
+  (new RawYAMLBuildStep())->buildAll();
+  (new MergedYAMLBuildStep())->buildAll();
+  (new MergedMarkdownBuildStep())->buildAll();
   (new GuidesHTMLBuildStep())->buildAll();
+  (new APIHTMLBuildStep())->buildAll();
+  (new BuildIDBuildStep())->buildAll();
 }
 
 hhvm_to_yaml();
