cb766a731c5849ed7ce66fa2dddc96db7e438397
Fred Emmott <fe@fb.com>
2015-11-12 16:13:15 -0800

    Split API index generation to a separate build step
    No real changes yet, just a tiny refactor.

--- src/build/APIHTMLBuildStep.php
+++ src/build/APIHTMLBuildStep.php
@@ -5,7 +5,6 @@ namespace HHVM\UserDocumentation;
 final class APIHTMLBuildStep extends AbstractMarkdownRenderBuildStep {
   const string SOURCE_ROOT = BuildPaths::APIDOCS_MARKDOWN;
   const string BUILD_ROOT = BuildPaths::APIDOCS_HTML;
-  const string METHOD_DELIM = "method";
 
   public function buildAll(): void {
     Log::i("\nAPIHTMLBuild");
@@ -15,9 +14,7 @@ final class APIHTMLBuildStep extends AbstractMarkdownRenderBuildStep {
       ->map($path ==> substr($path, strlen(self::SOURCE_ROOT) + 1));
     sort($sources);
 
-    $list = $this->renderFiles($sources);
-
-    $this->createIndex($list);
+    $this->renderFiles($sources);
   }
 
   private function renderFile(string $input): string {
@@ -40,72 +37,5 @@ final class APIHTMLBuildStep extends AbstractMarkdownRenderBuildStep {
       ->map($unescaped ==> escapeshellarg($unescaped));
     shell_exec(sprintf("%s %s > %s", ...$args));
     return $output;
-  }
-
-  private function createIndex(
-    Iterable<string> $list,
-  ): void {
-    $index = $this->generateIndexData($list);
-    /* TODO: Use hack-codegen once
-     * https://github.com/facebook/hack-codegen/issues/7 is addressed? */
-    $code = file_get_contents(__DIR__.'/../APIIndexData.hhi');
-
-    $re = $s ==> '/'.preg_quote($s, '/').'/';
-
-    $replacements = [
-      $re('<?hh // decl') => '<?php',
-      '/\): [^{]+{/' => ') {',
-      $re('/* CODEGEN GOES HERE */') => 'return '.var_export($index, true).';',
-    ];
-    foreach ($replacements as $pattern => $replacement) {
-      $code = preg_replace($pattern, $replacement, $code);
-    }
-    file_put_contents(
-      BuildPaths::APIDOCS_INDEX,
-      $code,
-    );
-  }
-
-  private function generateIndexData(
-    Iterable<string> $list,
-  ): APIIndexShape {
-    Log::i("\nCreate Index");
-    $out = [];
-    foreach ($list as $path) {
-      Log::v('.');
-      $path = str_replace(BuildPaths::APIDOCS_HTML.'/', '', $path);
-      $base_parts = explode('.', basename($path, '.html'), 2);
-
-      list($type, $api) = $base_parts;
-      if (!array_key_exists($type, $out)) {
-        $out[$type] = [];
-      }
-      $api_parts = explode('.', $api);
-
-      if (in_array(self::METHOD_DELIM, $api_parts, TRUE)) {
-        $method_sep_index = array_search(self::METHOD_DELIM, $api_parts, true);
-        $method_index = $method_sep_index + 1;
-        invariant(
-          array_key_exists($method_index, $api_parts),
-          "Method at key %s does not exist in %s filename",
-          $method_index,
-          $path,
-        );
-        $parent_api = rtrim(explode(self::METHOD_DELIM, $api)[0], ".");
-        $method = $api_parts[$method_index];
-
-        if (!array_key_exists($parent_api, $out[$type])) {
-          $out[$type][$parent_api] = ["path" => "", "methods" => []];
-        }
-        $out[$type][$parent_api]["methods"][$method] = $path;
-      } else {
-        if (!array_key_exists($api, $out[$type])) {
-          $out[$type][$api] = ["path" => "", "methods" => []];
-        }
-        $out[$type][$api]["path"] = $path;
-      }
-    }
-    // UNSAFE
-    return $out;
   }
 }
---
+++ src/build/APIIndexBuildStep.php
@@ -0,0 +1,86 @@
+<?hh // strict
+
+namespace HHVM\UserDocumentation;
+
+final class APIIndexBuildStep extends BuildStep {
+  const string METHOD_DELIM = "method";
+
+  public function buildAll(): void {
+    Log::i("\nAPIIndexBuild");
+    $sources = (self::findSources(BuildPaths::APIDOCS_MARKDOWN, Set{'md'})
+      ->filter($path ==> basename($path) !== 'README.md')
+      ->filter($path ==> strpos($path, '-examples') === false)
+      ->map($path ==> substr($path, strlen(BuildPaths::APIDOCS_MARKDOWN) + 1))
+      ->map($path ==> APIHTMLBuildStep::getOutputFileName($path))
+    );
+
+    $this->createIndex($sources);
+  }
+
+  private function createIndex(
+    Iterable<string> $list,
+  ): void {
+    $index = $this->generateIndexData($list);
+    /* TODO: Use hack-codegen once
+     * https://github.com/facebook/hack-codegen/issues/7 is addressed? */
+    $code = file_get_contents(__DIR__.'/../APIIndexData.hhi');
+
+    $re = $s ==> '/'.preg_quote($s, '/').'/';
+
+    $replacements = [
+      $re('<?hh // decl') => '<?php',
+      '/\): [^{]+{/' => ') {',
+      $re('/* CODEGEN GOES HERE */') => 'return '.var_export($index, true).';',
+    ];
+    foreach ($replacements as $pattern => $replacement) {
+      $code = preg_replace($pattern, $replacement, $code);
+    }
+    file_put_contents(
+      BuildPaths::APIDOCS_INDEX,
+      $code,
+    );
+  }
+
+  private function generateIndexData(
+    Iterable<string> $list,
+  ): APIIndexShape {
+    Log::i("\nCreate Index");
+    $out = [];
+    foreach ($list as $path) {
+      Log::v('.');
+      $path = str_replace(BuildPaths::APIDOCS_HTML.'/', '', $path);
+      $base_parts = explode('.', basename($path, '.html'), 2);
+
+      list($type, $api) = $base_parts;
+      if (!array_key_exists($type, $out)) {
+        $out[$type] = [];
+      }
+      $api_parts = explode('.', $api);
+
+      if (in_array(self::METHOD_DELIM, $api_parts, TRUE)) {
+        $method_sep_index = array_search(self::METHOD_DELIM, $api_parts, true);
+        $method_index = $method_sep_index + 1;
+        invariant(
+          array_key_exists($method_index, $api_parts),
+          "Method at key %s does not exist in %s filename",
+          $method_index,
+          $path,
+        );
+        $parent_api = rtrim(explode(self::METHOD_DELIM, $api)[0], ".");
+        $method = $api_parts[$method_index];
+
+        if (!array_key_exists($parent_api, $out[$type])) {
+          $out[$type][$parent_api] = ["path" => "", "methods" => []];
+        }
+        $out[$type][$parent_api]["methods"][$method] = $path;
+      } else {
+        if (!array_key_exists($api, $out[$type])) {
+          $out[$type][$api] = ["path" => "", "methods" => []];
+        }
+        $out[$type][$api]["path"] = $path;
+      }
+    }
+    // UNSAFE
+    return $out;
+  }
+}
