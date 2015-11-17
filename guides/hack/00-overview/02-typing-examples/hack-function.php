47c5a5865a4bb3366e9cf35723637c39f77d5d78
David Doran <daviddoran@fb.com>
2015-11-17 14:41:38 +0000

    Split out "Hack Overview: Typing" inline code examples
    In both PHP and HHVM the parentheses around an `if` statement's
    condition seem to be required.
    
    Part of issue #60

---
+++ guides/hack/00-overview/02-typing-examples/hack-function.php
@@ -0,0 +1,11 @@
+<?hh
+
+namespace Hack\UserDocumentation\Overview\Typing\Examples\HackFunction;
+
+function bar(int $a, string $b) {
+  if ($a > 0) {
+    echo '0' . $b;
+  } else {
+    echo (5 . $b);
+  }
+}
---
+++ guides/hack/00-overview/02-typing-examples/php-function.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hack\UserDocumentation\Overview\Typing\Examples\PHPFunction;
+
+function bar($a, $b) {
+  if ($a > 0) {
+    echo '0' . $b;
+  } else {
+    echo (5 . $b);
+  }
+}
