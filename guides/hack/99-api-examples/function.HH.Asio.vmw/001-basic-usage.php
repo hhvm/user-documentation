f36a662a2c521837414dd4607f97afa55944d219
Sara Golemon <sgolemon@fb.com>
2015-11-30 15:46:26 -0800

    Add basic usage example for \HH\Asio\vmw

---
+++ guides/hack/99-api-examples/function.HH.Asio.vmw/001-basic-usage.php
@@ -0,0 +1,27 @@
+<?hh
+
+namespace Hack\UserDocumentation\API\Examples\HH\Asio\vmw;
+
+// Map a vector of numbers to half integer half
+// throwing if they can't be divided evenly
+$halves = \HH\Asio\join(\HH\Asio\vmw(
+  Vector { 1, 2, 3, 4 },
+
+  async ($val) ==> {
+    if ($val % 2) {
+      throw new \Exception("$val is an odd number");
+    } else {
+      return $val / 2;
+    }
+  },
+));
+
+foreach($halves as $result) {
+  if ($result->isSucceeded()) {
+    echo "Success: ";
+    var_dump($result->getResult());
+  } else {
+    echo "Failed: ";
+    var_dump($result->getException()->getMessage());
+  }
+}
