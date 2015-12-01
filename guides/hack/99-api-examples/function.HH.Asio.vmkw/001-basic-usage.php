a5d107d00cf41194a3205a6c4643778dfdfabf16
Sara Golemon <sgolemon@fb.com>
2015-11-30 16:02:11 -0800

    Add basic usage example for \HH\Asio\vmkw

---
+++ guides/hack/99-api-examples/function.HH.Asio.vmkw/001-basic-usage.php
@@ -0,0 +1,29 @@
+<?hh
+
+namespace Hack\UserDocumentation\API\Examples\HH\Asio\vmkw;
+
+// Map a vector of numbers to their value divided by their index
+// throwing on division by zero.
+$quotients = \HH\Asio\join(\HH\Asio\vmkw(
+  Vector { 1, 2, 6, 12 },
+
+  async ($idx, $val) ==> {
+    if ($idx != 0) {
+      return $val / $idx;
+    } else {
+      throw new \Exception("Division by zero: " .
+        print_r($val, true) . '/' . print_r($idx, true)
+      );
+    }
+  },
+));
+
+foreach($quotients as $result) {
+  if ($result->isSucceeded()) {
+    echo "Success: ";
+    var_dump($result->getResult());
+  } else {
+    echo "Failed: ";
+    var_dump($result->getException()->getMessage());
+  }
+}
