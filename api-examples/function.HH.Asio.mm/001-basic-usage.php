426323aabd5b4ff7e4b9aee2be5e425b1b91331a
Sara Golemon <sgolemon@fb.com>
2015-12-01 10:57:27 -0800

    Add basic usage exqample for \HH\Asio\mm

---
+++ api-examples/function.HH.Asio.mm/001-basic-usage.php
@@ -0,0 +1,34 @@
+<?hh
+
+namespace Hack\UserDocumentation\API\Examples\HH\Asio\mm;
+
+/**
+ * Query an arbitrary number of URLs in parallel
+ * returning them as a Map of string responses.
+ *
+ * Refer to \HH\Asio\m() for a more verbose version of this.
+ */
+async function get_urls(
+  \ConstMap<string, string> $urls,
+): Awaitable<Map<string, string>> {
+
+  // Invoke \HH\Asio\curl_exec for each URL,
+  // then await on each in parallel
+  return await \HH\Asio\mm(
+    $urls,
+    fun("\HH\Asio\curl_exec"),
+  );
+}
+
+$urls = ImmMap {
+  'com' => "http://example.com",
+  'net' => "http://example.net",
+  'org' => "http://example.org",
+};
+
+$pages = \HH\Asio\join(get_urls($urls));
+foreach ($pages as $name => $page) {
+  echo $name, ': ';
+  echo substr($page, 0, 15) . ' ... ' . substr($page, -8);
+}
+
