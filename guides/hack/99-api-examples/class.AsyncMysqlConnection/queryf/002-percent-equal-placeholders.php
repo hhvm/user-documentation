17b90a11df4944c0e652edd2d7e96a30442285b3
Joel Marcey <joelm@fb.com>
2015-11-30 16:05:39 -0800

    Example with %= placeholder for queryf
    Fixes #71

--- guides/hack/99-api-examples/class.AsyncMysqlConnection/queryf/002-percent-equal-placeholders.php
+++ guides/hack/99-api-examples/class.AsyncMysqlConnection/queryf/002-percent-equal-placeholders.php
@@ -1,6 +1,6 @@
 <?hh
 
-namespace Hack\UserDocumentation\API\Examples\AsyncMysql\Conn\Queryf;
+namespace Hack\UserDocumentation\API\Examples\AsyncMysql\Conn\Queryf\PerEqual;
 
 require __DIR__ .'/../../__includes/async_mysql_connect.inc.php';
 
@@ -17,21 +17,25 @@ async function connect(\AsyncMysqlConnectionPool $pool):
   );
 }
 
-async function get_data(\AsyncMysqlConnection $conn, string $col, int $id):
+async function get_data(
+  \AsyncMysqlConnection $conn,
+  string $col,
+  ?string $email):
   Awaitable<\AsyncMysqlQueryResult> {
+  // %=s allows you to check an actual string value or IS NULL
   return await $conn->queryf(
-    'SELECT %C FROM test_table where userID = %d',
+    'SELECT %C FROM test_table where email %=s',
     $col,
-    $id
+    $email
   );
 }
 
 async function simple_queryf(): Awaitable<int> {
   $pool = new \AsyncMysqlConnectionPool(array());
   $conn = await connect($pool);
-  $result = await get_data($conn, 'name', 1);
+  $result = await get_data($conn, 'name', 'joelm@fb.com');
   $x = $result->numRows();
-  $result = await get_data($conn, 'name', 2);
+  $result = await get_data($conn, 'name', null);
   $conn->close();
   return $x + $result->numRows();
 }
