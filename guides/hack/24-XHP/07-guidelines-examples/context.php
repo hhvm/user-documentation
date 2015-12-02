696cfbb16856ff42016670f8a2aabc4d64c9b1e7
Fred Emmott <fe@fb.com>
2015-12-02 15:11:24 -0800

    Fix XHP contexts example
    Parent never rendered children
    
    cc @joelm

--- guides/hack/24-XHP/07-guidelines-examples/context.php
+++ guides/hack/24-XHP/07-guidelines-examples/context.php
@@ -5,14 +5,22 @@ require __DIR__ . "/../../../../vendor/autoload.php";
 class :ui-myparent extends :x:element {
   attribute string text @required;
   children (:ui-mychild);
+
   protected function render(): XHPRoot {
-    return
-      <x:frag>{$this->:text}</x:frag>;
+    return (
+      <dl>
+        <dt>Text</dt>
+        <dd>{$this->:text}</dd>
+        <dt>Child</dt>
+        <dd>{$this->getChildren()}</dd>
+      </dl>
+    );
   }
 }
 
 class :ui-mychild extends :x:element {
   attribute string text @required;
+
   protected function render(): XHPRoot {
     if ($this->getContext('hint') === 'Yes') {
       return
@@ -27,10 +35,9 @@ function guidelines_examples_context_run(string $s): void {
   $child = <ui-mychild text="Go" />;
   $parent = <ui-myparent text={$s}>{$child}</ui-myparent>;
   $parent->setContext('hint', $s);
-  echo
-    "There seems to be a bug here. This should work, I think. " .
-    "But, the child context is not getting set via propagation.";
+  echo $parent;
 }
 
 guidelines_examples_context_run('No');
+echo "\n\n";
 guidelines_examples_context_run('Yes');
