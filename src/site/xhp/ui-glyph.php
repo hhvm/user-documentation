73d668aead2404407588ce314158c01fa30caaeb
Fred Emmott <fe@fb.com>
2015-12-01 16:10:01 -0800

    Tabs to 2space

--- src/site/xhp/ui-glyph.php
+++ src/site/xhp/ui-glyph.php
@@ -6,7 +6,7 @@ final class :ui:glyph extends :x:element {
   attribute UIGlyphIcon icon @required;
 
   protected function render(): XHPRoot {
-	  $class = "glyphIcon fa fa-".$this->:icon;
+    $class = "glyphIcon fa fa-".$this->:icon;
     return <i class={$class}></i>;
   }
 }
