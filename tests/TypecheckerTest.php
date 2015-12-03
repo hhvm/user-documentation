9babafff66b6f8af5e647c9908f7b47c0eb172bd
Fred Emmott <fe@fb.com>
2015-12-03 11:02:47 -0800

    Add PHPUnit test that runs the typechecker against the whole tree
    refs #157
    
        hhvm vendor/bin/phpunit tests/

---
+++ tests/TypecheckerTest.php
@@ -0,0 +1,16 @@
+<?hh
+
+namespace HHVM\UserDocumentation\Tests;
+
+class TypecheckerTest extends \PHPUnit_Framework_TestCase {
+  public function testTypecheckerPasses(): void {
+    $output = array();
+    $exit_code = null;
+    exec(
+      'hh_server --check '.escapeshellarg(__DIR__.'/../').' 2>&1',
+      $output,
+      $exit_code,
+    );
+    $this->assertSame(0, $exit_code, implode("\n", $output));
+  }
+}
