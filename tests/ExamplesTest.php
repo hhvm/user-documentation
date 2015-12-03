e49b2e0b37fad4bdb074a9202672b0959e97c23f
Fred Emmott <fe@fb.com>
2015-12-03 13:51:22 -0800

    Add test that runs all the examples (sorry, it's slow)
    It also fails for now, fixing.

---
+++ tests/ExamplesTest.php
@@ -0,0 +1,35 @@
+<?hh
+
+namespace HHVM\UserDocumentation\Tests;
+
+use HHVM\UserDocumentation\LocalConfig;
+
+class ExamplesTest extends \PHPUnit_Framework_TestCase {
+  const string TEST_RUNNER = LocalConfig::HHVM_TREE.'/hphp/test/run';
+
+  public function testExamplesOutput(): void {
+    $this->runExamples(Vector { });
+  }
+
+  public function testExamplesTypecheck(): void {
+    $this->runExamples(Vector { '--typechecker' });
+  }
+
+  private function runExamples(Vector<string> $extra_args): void {
+    $command = Vector {
+      PHP_BINARY,
+      self::TEST_RUNNER,
+      '--hhvm-binary-path', PHP_BINARY,
+      '-m', 'interp',
+    };
+    $command->addAll($extra_args);
+    $command[] = LocalConfig::ROOT.'/guides';
+
+    $command_str = implode(' ', $command->map($arg ==> escapeshellarg($arg)));
+    $exit_code = null;
+    $output = null;
+    exec($command_str, /*&*/ $output, /*&*/ $exit_code);
+
+    $this->assertSame(0, $exit_code, implode("\n", $output));
+  }
+}
