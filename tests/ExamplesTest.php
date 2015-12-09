e4a76ddf05a496151f7c814333614a388bb527db
Fred Emmott <fe@fb.com>
2015-12-09 10:10:23 -0800

    Make typechecker tests for examples run without build tree

--- tests/ExamplesTest.php
+++ tests/ExamplesTest.php
@@ -8,24 +8,38 @@ class ExamplesTest extends \PHPUnit_Framework_TestCase {
   const string TEST_RUNNER = LocalConfig::HHVM_TREE.'/hphp/test/run';
 
   public function testExamplesOutput(): void {
-    $this->runExamples(Vector { });
+    $this->runExamples(Vector {
+    });
   }
 
   public function testExamplesTypecheck(): void {
-    $hh_server = LocalConfig::HHVM_TREE.'/hphp/hack/bin/hh_server';
+    $hh_server = dirname(PHP_BINARY).'/hh_server';
     if (!file_exists($hh_server)) {
-      $this->markTestSkipped(
-        "hphp/test/run --typechecker needs a built hack tree"
-      );
+      $this->markTestSkipped("Couldn't find hh_server");
     }
-    $this->runExamples(Vector { '--typechecker' });
+
+    $this->runExamples(Vector {
+      '--typechecker',
+    });
+  }
+
+  private function getHHVMPath(): string {
+    return PHP_BINARY;
+  }
+
+  <<__Memoize>>
+  private function getHHServerPath(): string {
+    $hh_server = dirname(PHP_BINARY).'/hh_server';
+    if (!file_exists($hh_server)) {
+      $this->markTestSkipped("Couldn't find hh_server");
+    }
+    return $hh_server;
   }
 
   private function runExamples(Vector<string> $extra_args): void {
     $command = Vector {
       PHP_BINARY,
       self::TEST_RUNNER,
-      '--hhvm-binary-path', PHP_BINARY,
       '-m', 'interp',
     };
     $command->addAll($extra_args);
@@ -34,6 +48,15 @@ class ExamplesTest extends \PHPUnit_Framework_TestCase {
     $command_str = implode(' ', $command->map($arg ==> escapeshellarg($arg)));
     $exit_code = null;
     $output = null;
+
+    $env = Vector {
+      'HHVM_BIN='.escapeshellarg($this->getHHVMPath()),
+      'HH_SERVER_BIN='.escapeshellarg($this->getHHServerPath()),
+    };
+
+    $command_str =
+      implode('', $env->map($x ==> $x.' ')).$command_str;
+
     exec($command_str, /*&*/ $output, /*&*/ $exit_code);
 
     $this->assertSame(0, $exit_code, implode("\n", $output));
