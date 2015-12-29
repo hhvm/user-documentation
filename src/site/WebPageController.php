2dc9742e4e188440bca8e92367cfbc34e2066e95
Fred Emmott <fe@fb.com>
2015-12-29 11:40:09 -0800

    Enable page zoom on mobile
    Zooming messes up the layout, but initial zoom is 1. Slightly messed up layout
    on zoom, but that's better than just plain not being able to read the
    text...
    
    cc @sgolemon :p

--- src/site/WebPageController.php
+++ src/site/WebPageController.php
@@ -88,7 +88,10 @@ EOF;
           <head>
             <title>{$title}</title>
             <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
             <link rel="shortcut icon" href="/favicon.png" />
             {$open_search}
             <x:comment>
