25fec5da37f6e03d032f6f697f5c81345a91e4cf
Fred Emmott <fe@fb.com>
2016-04-28 14:48:37 -0700

    Don't set the variable for auth token

--- bin/deploy-to-staging.sh
+++ bin/deploy-to-staging.sh
@@ -12,9 +12,7 @@ IMAGE_TAG=$(date +%Y-%m-%d)-$DEPLOY_REV
 IMAGE_NAME=hhvm/user-documentation:$IMAGE_TAG
 
 echo "** Building image"
-COMPOSER_GITHUB_OAUTH_TOKEN=$(hhvm bin/composer-github-oauth-token.php)
 docker build \
-  --build-arg COMPOSER_GITHUB_OAUTH_TOKEN="$COMPOSER_GITHUB_OAUTH_TOKEN" \
   -t $IMAGE_NAME $(pwd)
 docker tag -f $IMAGE_NAME hhvm/user-documentation:latest # add an alias
 
