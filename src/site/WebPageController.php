77bb13939ec50763d75bf307c6d359f042ebe4ef
Joel Marcey <joelm@fb.com>
2016-05-18 09:04:37 -0700

    Replace our custom search with Algolia search (#322)
    Fixes #312

--- src/site/WebPageController.php
+++ src/site/WebPageController.php
@@ -93,6 +93,7 @@ EOF;
               content="width=device-width, initial-scale=1.0"
             />
             <link rel="shortcut icon" href="/favicon.png" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.css" />
             {$open_search}
             <x:comment>
               Build ID: {file_get_contents(BuildPaths::BUILD_ID)}
@@ -113,6 +114,8 @@ EOF;
             {$this->getSideNav()}
             {$content}
             {$this->getEagerFetchScript()}
+            {$this->getAlgoliaDocSearchScript()}
+            {$this->getCallToAlgoliaDocSearchScript()}
           </body>
         </html>
       </x:doctype>;
@@ -225,10 +228,9 @@ EOF;
               report a problem or make a suggestion
             </github-issue-link>
           </div>
-          <search-bar
-            class="headerElement"
-            placeholder="Search our Documentation"
-          />
+          <div class="headerElement algolia-search-wrapper">
+            <input id="algolia-doc-search" type="search" placeholder="Search docs..." />
+          </div>
         </div>
       </div>;
   }
@@ -354,6 +356,22 @@ if (document.querySelectorAll) {
     );
   }
 }
+EOF;
+    return <script language="javascript">{$code}</script>;
+  }
+
+  private function getAlgoliaDocSearchScript(): :script {
+    return <script type="text/javascript" src="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.js"></script>;
+  }
+
+  private function getCallToAlgoliaDocSearchScript(): :script {
+    $code = <<<EOF
+// For Algolia search
+docsearch({
+  apiKey: '07e0af547f8ec07e210176de2f89e19f',
+  indexName: 'hhvm',
+  inputSelector: '#algolia-doc-search',
+});
 EOF;
     return <script language="javascript">{$code}</script>;
   }
