67da9c6e68a1922ca9558986700c66f7e39237cc
Fred Emmott <fe@fb.com>
2017-08-30 10:59:40 -0700

    Manually force search results for vec/dict/keyset

--- src/site/controllers/SearchController.php
+++ src/site/controllers/SearchController.php
@@ -4,10 +4,13 @@ use Facebook\HackRouter\StringRequestParameter;
 use Facebook\HackRouter\StringRequestParameterSlashes;
 use HHVM\UserDocumentation\APIIndex;
 use HHVM\UserDocumentation\GuidesIndex;
+use HHVM\UserDocumentation\GuidesProduct;
 use HHVM\UserDocumentation\PHPAPIIndex;
 use HHVM\UserDocumentation\SearchResultSet;
 
 use Psr\Http\Message\ServerRequestInterface;
+
+use namespace HH\Lib\{C, Str};
 
 final class SearchController extends WebPageController {
   use SearchControllerParametersTrait;
@@ -88,9 +91,33 @@ final class SearchController extends WebPageController {
 
   private function getSearchResults(): SearchResultSet {
     return ((new SearchResultSet())
+      ->addAll($this->getHardcodedResults())
       ->addAll(APIIndex::search($this->getSearchTerm()))
       ->addAll(GuidesIndex::search($this->getSearchTerm()))
       ->addAll(PHPAPIIndex::search($this->getSearchTerm()))
     );
+  }
+
+  private function getHardcodedResults(): SearchResultSet {
+    $term = Str\lowercase($this->getSearchTerm());
+    $results = new SearchResultSet();
+
+    $hack_array_keywords = keyset[
+      'vec',
+      'dict',
+      'keyset',
+      'vector', 'immvector', 'constvector',
+      'map', 'immmap', 'constmap',
+      'set', 'immset', 'constset',
+    ];
+    if (C\contains_key($hack_array_keywords, $term)) {
+      $results->addGuideResult(
+        GuidesProduct::HACK,
+        'collections',
+        'hack-arrays',
+      );
+    }
+
+    return $results;
   }
 }
