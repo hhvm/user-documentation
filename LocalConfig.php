7eecf8d0fe3e36cbccacef8deb9855bcbc793e09
Fred Emmott <fe@fb.com>
2017-10-20 15:29:47 -0700

    Move LocalConfig.php back up a level
    means we can keep using constants for ROOT - if it's in a subdir, we need '..', which gives us a non-normalized path unless we call realpath

--- LocalConfig.php
+++ LocalConfig.php
@@ -3,7 +3,7 @@
 namespace HHVM\UserDocumentation;
 
 abstract final class LocalConfig {
-  const string ROOT = __DIR__.'/..';
+  const string ROOT = __DIR__;
 
   const string BUILD_DIR = self::ROOT.'/build';
   const string HHVM_TREE = self::ROOT.'/../hhvm';
