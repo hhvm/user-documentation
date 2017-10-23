c05fc64a80b4269d952f33e7532af4ed4bc79362
Fred Emmott <fe@fb.com>
2017-10-23 15:24:43 -0700

    Use hack-router and hack-router-codegen v0.9

--- src/site/Router.php
+++ src/site/Router.php
@@ -4,12 +4,7 @@ use FredEmmott\TypeAssert\TypeAssert;
 use HHVM\UserDocumentation\BuildPaths;
 use HHVM\UserDocumentation\LocalConfig;
 
+use namespace HH\Lib\Dict;
+
 final class Router extends RouterCodegenBase {
-  <<__Override>>
-  protected function getCacheFilePath(): ?string {
-    if (\Facebook\AutoloadMap\Generated\is_dev()) {
-      return null;
-    }
-    return BuildPaths::FASTROUTE_CACHE;
-  }
 }
