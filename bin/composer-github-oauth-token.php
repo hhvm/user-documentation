29d3f8dc1f93d1dc8b3710685eb0e9a8b229bd6b
Fred Emmott <fe@fb.com>
2017-11-16 13:51:05 -0800

    add missing copyright headers

--- bin/composer-github-oauth-token.php
+++ bin/composer-github-oauth-token.php
@@ -1,4 +1,13 @@
 <?hh
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
 
 // The deploy process can use this to get higher rate limits
 function dump_composer_github_oauth_token(): void {
