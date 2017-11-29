13b32e27b0854247d9efe47a8a4d1ee4b4f17cf3
Fred Emmott <fe@fb.com>
2017-11-29 14:52:28 -0800

    remove dead code from APIHTMLBuildStep

--- src/build/APIHTMLBuildStep.php
+++ src/build/APIHTMLBuildStep.php
@@ -26,28 +26,4 @@ final class APIHTMLBuildStep extends AbstractMarkdownRenderBuildStep {
 
     $this->renderFiles($sources);
   }
-
-  private function renderFile(string $input): string {
-    $input = str_replace(self::SOURCE_ROOT.'/', '', $input);
-    $parts = (new Vector(explode('/', $input)))
-      ->map(
-        $part ==> preg_match('/^[0-9]{2}-/', $part) ? substr($part, 3) : $part
-      );
-
-    $output = implode('/', $parts);
-    $output = basename($output, '.md').'.html';
-    $output = BuildPaths::APIDOCS_HTML.'/'.$output;
-
-    $output_dir = dirname($output);
-    if (!is_dir($output_dir)) {
-      mkdir($output_dir, /* mode = */ 0755, /* recursive = */ true);
-    }
-
-    $input = realpath(self::SOURCE_ROOT.'/'.$input);
-    $args = (Vector {self::RENDERER, $input, $output})
-      ->map($unescaped ==> escapeshellarg($unescaped));
-      var_dump($args);
-    shell_exec(sprintf("%s %s > %s", ...$args));
-    return $output;
-  }
 }
