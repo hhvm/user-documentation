ad79d5605787a3df957c203d19b2184225bee367
Fred Emmott <fe@fb.com>
2017-11-29 12:49:56 -0800

    skeletons for everything

---
+++ src/gfm/inlines/Emphasis.php
@@ -0,0 +1,23 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+namespace Facebook\GFM\Inlines;
+
+use namespace HH\Lib\Str;
+
+final class Emphasis extends Inline {
+  public static function consume(
+    Context $_,
+    string $string,
+  ): ?(Inline, string) {
+    return null; // FIXME
+  }
+}
