5805e5198f85ef3a658310a8a96c8c51a84faba1
Fred Emmott <fe@fb.com>
2017-12-12 12:59:05 -0800

    [gfm spec] make sure we get the expected number of examples.

--- tests/FBMarkdownTest.php
+++ tests/FBMarkdownTest.php
@@ -8,54 +8,67 @@ use namespace HH\Lib\{C, Str, Vec};
 use function Facebook\FBExpect\expect;
 
 class FBMarkdownTest extends \PHPUnit_Framework_TestCase {
+  const string EXAMPLE_START = "\n```````````````````````````````` example";
+  const string EXAMPLE_END = "\n````````````````````````````````";
   const string TAB_REPLACEMENT = "\u{2192}";
-  public function getExamples(): array<(string, string)> {
+  // Sanity check - make sure it matches the last one in the spec
+  const int EXAMPLE_COUNT = 649;
+
+  public function getExamples(): array<(string, string, string)> {
     $spec_file_path = \getenv('GFM_SPEC_FILE');
     if (!is_string($spec_file_path)) {
       $this->markTestSkipped('need GFM_SPEC_FILE env var');
     }
-    $parser_ctx = (new ParserContext())->enableHTML_UNSAFE();
-    $doc = parse($parser_ctx, \file_get_contents($spec_file_path));
 
-    return $doc->getChildren()
-      |> Vec\filter(
-        $$,
-        $child ==> $child instanceof Blocks\CodeBlock
-        && Str\trim((string) $child->getInfoString()) === 'example'
-      )
-      |> Vec\map(
-        $$,
-        $child ==>
-          TypeAssert\instance_of(Blocks\CodeBlock::class, $child)->getCode(),
-      )
-      |> Vec\map(
-        $$,
-        $example ==> Str\replace($example, self::TAB_REPLACEMENT, "\t"),
-      )
-      |> Vec\map_with_key(
-        $$,
-        ($i, $example) ==> {
-          $parts = Str\split($example, "\n.\n");
-          $count = C\count($parts);
-          invariant(
-            $count === 1 || $count === 2,
-            "examples should have input and output, example %d has %d parts",
-            $i + 1,
-            $count,
-          );
-          if ($count === 1) {
-            $expected = '';
-          } else {
-            $expected = $parts[1]."\n";
-          }
-          return tuple($parts[0], $expected);
-        }
-      )
-      |> /* HH_IGNORE_ERROR[4007] */ (array) $$;
+    $text = \file_get_contents($spec_file_path);
+    $raw_examples = vec[];
+    $offset = 0;
+    while (true) {
+      $start = Str\search($text, self::EXAMPLE_START, $offset);
+      if ($start === null) {
+        break;
+      }
+      $start += Str\length(self::EXAMPLE_START);
+      $start = Str\search($text, "\n", $start);
+      invariant($start !== null, "No newline after start marker");
+      $end = Str\search($text, self::EXAMPLE_END, $start);
+      invariant($end !== null, 'Found start without end at %d', $offset);
+
+      $raw_examples[] = Str\slice(
+        $text,
+        $start,
+        ($end - $start) + 1,
+      );
+      $offset = $end + Str\length(self::EXAMPLE_END);
+    }
+
+    $examples = [];
+
+    foreach ($raw_examples as $example) {
+      $parts = Str\split($example, "\n.\n");
+      $count = C\count($parts);
+      invariant(
+        $count === 1 || $count === 2,
+        "examples should have input and output, example %d has %d parts",
+        C\count($examples) + 1,
+        $count,
+      );
+      $examples[] = tuple(
+        'Example '.(C\count($examples) + 1),
+        Str\replace($parts[0], self::TAB_REPLACEMENT, "\t"),
+        $parts[1] ?? '',
+      );
+    }
+    expect(C\count($examples))->toBeSame(
+      self::EXAMPLE_COUNT,
+      "Did not get the expected number of examples",
+    );
+    return $examples;
   }
 
   /** @dataProvider getExamples */
   public function testExample(
+    string $name,
     string $in,
     string $expected_html,
   ): void {
@@ -70,7 +83,8 @@ class FBMarkdownTest extends \PHPUnit_Framework_TestCase {
 
     expect($actual_html)->toBeSame(
       $expected_html,
-      "HTML differs for markdown:\n%s",
+      "HTML differs for %s:\n%s",
+      $name,
       $in,
     );
   }
