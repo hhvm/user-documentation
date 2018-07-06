0afe35d8f6fa936bb97d49ea0448dab4d6a1b159
Fred Emmott <fe@fb.com>
2018-07-06 14:00:50 -0700

    Fix missed refactoring in index.php

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ require_once(__DIR__.'/../vendor/hh_autoload.php');
 $request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
 
 HH\Asio\join(
-  HHVMDocumentationSite::respondTo($request)
+  HHVMDocumentationSite::respondToAsync($request)
 );
