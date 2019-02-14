435c108272cc5ec41f00eca1c108f2c955c99f72
Fred Emmott <fe@fb.com>
2019-02-14 08:27:23 -0800

    update .travis.sh for 4.0

--- .travis.sh
+++ .travis.sh
@@ -3,19 +3,15 @@ set -ex
 hhvm --version
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install -y ruby bundler
+apt-get install -y ruby bundler php-cli unzip zip
 
-curl https://getcomposer.org/installer | hhvm -d hhvm.jit=0 --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
+curl https://getcomposer.org/installer | php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
 cd /var/source
 git submodule update --init
-hhvm -d hhvm.jit=0 /usr/local/bin/composer install
+php /usr/local/bin/composer install
 bundle install --path vendor-rb
 
 hh_client
 hhvm bin/build.php
 hhvm vendor/bin/hacktest tests/
-
-grep ignored_paths .hhconfig > hhconfig
-mv hhconfig .hhconfig
-hh_server --check $(pwd)
