40f6b9bfc3b8236787115d3f3a8d9517d2f6d3eb
Fred Emmott <fe@fb.com>
2019-03-14 10:21:54 -0700

    Set request host, port, and scheme
    fixes #608
    fixes #609
    refs usox/hackttp#20

--- public/index.php
+++ public/index.php
@@ -1,10 +1,39 @@
 <?hh // strict
 
-require_once(__DIR__.'/../vendor/hh_autoload.php');
+require_once(__DIR__.'/../vendor/autoload.hack');
+
+use namespace HH\Lib\Str;
 
 <<__EntryPoint>>
 async function site_main_async(): Awaitable<noreturn> {
+  \Facebook\AutoloadMap\initialize();
   $request = Usox\HackTTP\createServerRequestFromGlobals();
+  // header is of the form HOST:PORT - for `parse_url`, we need
+  // `http://HOST:port/` as `parse_url()` is unable to parse IPv6
+  // strings like `[::1]:8080
+  /* HH_FIXME[2050] $_SERVER */
+  $dummy_uri = 'http://'.$_SERVER['HTTP_HOST'].'/';
+  $host = \parse_url($dummy_uri, PHP_URL_HOST);
+  $port = \parse_url($dummy_uri, PHP_URL_PORT);
+  // We just use this to figure out if we should do a redirect - if we were
+  // doing something more important, we should make sure that the remote end
+  // is from a local-use IP range.
+  $https = /* HH_FIXME[2050] */ $_SERVER['HTTPS'] ??
+    /* HH_FIXME[2050] */ $_SERVER['https'] ??
+    /* HH_FIXME[2050] */ $_SERVER['HTTP_X_FORWARDED_PROTO'] ??
+    false;
+  if ($https is string) {
+    $https = Str\lowercase((string)$https);
+    $https = $https === 'on' ||
+      $https === 'true' ||
+      $https === 'yes' ||
+      $https === 'https';
+  }
+  $scheme = ($https as bool) ? 'https' : 'http';
+  $request = $request->withUri(
+    $request->getUri()->withHost($host)->withPort($port)->withScheme($scheme),
+  );
+
   await HHVMDocumentationSite::respondToAsync($request);
   exit(0);
 }
