7b49c9dd37d5dd81a9a22ed1c0f1c79d4ab24174
Fred Emmott <fe@fb.com>
2019-06-05 12:30:58 -0700

    automatic changes for HHVM 4.7 to 4.8

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,11 +2,11 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<375d609aef1e0f28dff874e95167bdf6>>
+ * @generated SignedSource<<4390e400348db5153dd41fc61bdccdbc>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.7.0',
+  APIProduct::HACK => 'HHVM-4.8.0',
   APIProduct::HSL => 'v4.7.0',
 ];
