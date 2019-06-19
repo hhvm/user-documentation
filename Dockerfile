e42900db49cc4652bacaa663dff4d08429b91d99
Fred Emmott <fe@fb.com>
2019-06-19 10:33:27 -0700

    Update to HHVM 4.10

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM hhvm/hhvm-proxygen:4.8-latest
+FROM hhvm/hhvm-proxygen:4.10-latest
 ARG DOCKER_BUILD_ENV=prod
 ENV TZ UTC
 
--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,11 +2,11 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<4390e400348db5153dd41fc61bdccdbc>>
+ * @generated SignedSource<<91e7de261629de7925086e30f56b8c48>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.8.0',
+  APIProduct::HACK => 'HHVM-4.10.0',
   APIProduct::HSL => 'v4.7.0',
 ];
