e1ff9c6b1f04868893986f8a46db9e3dbc97e531
jjergus <jjergus@fb.com>
2019-07-24 14:22:35 -0700

    target HHVM 4.15

--- .deploy/prod.Dockerfile
+++ .deploy/prod.Dockerfile
@@ -1,6 +1,6 @@
 # Creates a docker image with a built copy of the site. Not repo-auth.
 # Useful as a scratch/testing area.
-FROM hhvm/hhvm-proxygen:4.14-latest
+FROM hhvm/hhvm-proxygen:4.15-latest
 
 ADD hhvm.prod.ini /etc/hhvm/site.ini
 ADD hhvm.hhbc /var/www/hhvm.hhbc
--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,11 +2,11 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<8664488d1985e7ba9e0fe366ea5f2bc5>>
+ * @generated SignedSource<<1ebfa7b97cf1b8e95dab4e35594e0d02>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.14.0',
+  APIProduct::HACK => 'HHVM-4.15.0',
   APIProduct::HSL => 'v4.7.0',
 ];
