711a87c7d8a079777d1cdb3ed61cfab60e9f9755
jjergus <jjergus@fb.com>
2019-11-06 10:05:00 -0800

    target HHVM 4.30

--- .deploy/prod.Dockerfile
+++ .deploy/prod.Dockerfile
@@ -1,4 +1,4 @@
-FROM hhvm/hhvm-proxygen:4.29-latest
+FROM hhvm/hhvm-proxygen:4.30-latest
 
 ADD hhvm.prod.ini /etc/hhvm/site.ini
 ADD hhvm.hhbc /var/www/hhvm.hhbc
--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,11 +2,11 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<f9992fdb2a75c7d58acf67a4872adfdb>>
+ * @generated SignedSource<<209bd673560ff822c090e327da20c774>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.29.0',
+  APIProduct::HACK => 'HHVM-4.30.0',
   APIProduct::HSL => 'v4.25.2',
 ];
