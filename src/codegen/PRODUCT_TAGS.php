356efa48aeb5a13379ad0034109e4f9f191dc5e4
Fred Emmott <fe@fb.com>
2020-01-27 14:36:56 -0800

    Update to HHVM 4.42 and hsl-experimental 4.37.1

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<ac13730a435f6a48eedf57511604c0ea>>
+ * @generated SignedSource<<5586ff6e39887e994b64ba462ebecbc7>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.40.0',
+  APIProduct::HACK => 'HHVM-4.42.0',
   APIProduct::HSL => 'v4.40.0',
-  APIProduct::HSL_EXPERIMENTAL => 'v4.37.0',
+  APIProduct::HSL_EXPERIMENTAL => 'v4.37.1',
 ];
