2043975b0229d2e26d40f34506bc2cd876ca5d3f
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-03-09 22:57:11 +0100

    Typo + Faster native shape validation

--- src/utils/_private/SuperGlobals.php
+++ src/utils/_private/SuperGlobals.php
@@ -2,7 +2,7 @@
 
 namespace HHVM\UserDocumentation\_Private\SuperGlobals;
 
-use namespace Facebook\{TypeAssert, TypeCoerce};
+use namespace Facebook\TypeAssert;
 
 function environment_variables(): dict<string, arraykey> {
   return TypeAssert\matches<dict<string, arraykey>>(
@@ -15,11 +15,11 @@ function environment_variables(): dict<string, arraykey> {
  * Add the keys that you need.
  * The more you add, the slower it becomes however.
  */
-type TServerVarabies = shape(
+type TServerVariables = shape(
   'HTTP_HOST' => string,
   ...
 );
 
-function server_variables(): TServerVarabies {
-  return TypeCoerce\match<TServerVarabies>(\HH\global_get('_SERVER'));
+function server_variables(): TServerVariables {
+  return \HH\global_get('_SERVER') as TServerVariables;
 }
