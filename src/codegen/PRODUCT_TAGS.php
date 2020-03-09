332d31bdfcedb0249b9db27aa496b57e22ddaafc
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-03-09 22:38:57 +0100

    Upgrade using the upgrade script

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<4b188fcc0cbb2f5e658fe946e6e6a754>>
+ * @generated SignedSource<<95cbc4b82b21111dba5cc97b206d366a>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.47.0',
+  APIProduct::HACK => 'HHVM-4.48.0',
   APIProduct::HSL => 'v4.40.0',
-  APIProduct::HSL_EXPERIMENTAL => 'v4.37.1',
+  APIProduct::HSL_EXPERIMENTAL => 'v4.37.2',
 ];
