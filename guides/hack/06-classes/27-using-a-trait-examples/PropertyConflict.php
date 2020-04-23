f371e97fb08881c2ff8bbb1491ffdf8460610cad
Francesco Zappa Nardelli <fzn@fb.com>
2020-04-23 11:57:37 +0000

    step 1 of adding examples (in progress)

---
+++ guides/hack/06-classes/27-using-a-trait-examples/PropertyConflict.php
@@ -0,0 +1,35 @@
+<?hh // strict
+
+namespace Hack\UserDocumentation\Classes\Traits\Examples\PropertyConflict;
+
+trait T1 {
+  public static int $x = 0;
+
+  public static function even() : void {
+    invariant(static::$x % 2 == 0, 'error, not even\n');
+    static::$x = static::$x + 2;
+  }
+}
+
+trait T2 {
+  public static int $x = 0;
+
+  public static function inc() : void {
+    static::$x = static::$x + 1;
+  }
+}
+
+class C {
+  use T1;
+  use T2;
+
+  public static function foo() : void {
+    static::inc();
+    static::even();
+  }
+}
+
+<<__EntryPoint>>
+function main() : void {
+  C::foo();
+}
