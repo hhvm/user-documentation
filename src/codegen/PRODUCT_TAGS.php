c6a65f7af6fe98aa6c3b433c5dd9793551f150d5
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-05-09 21:45:24 +0200

    Run update-versions and fix two tests
    Relies on a single change in usox/hackttp, readRawBlocking() -> read()

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<0355a0f6bc4cfaae712c509fe964945f>>
+ * @generated SignedSource<<dab5ba32cc3f4076ce29bfdd31b6539e>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.53.0',
-  APIProduct::HSL => 'v4.40.0',
-  APIProduct::HSL_EXPERIMENTAL => 'v4.50.1',
+  APIProduct::HACK => 'HHVM-4.56.0',
+  APIProduct::HSL => 'v4.41.0',
+  APIProduct::HSL_EXPERIMENTAL => 'v4.52.0',
 ];
