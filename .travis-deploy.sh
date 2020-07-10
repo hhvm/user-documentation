59b968a6cfa870e8c414b1f4cc1a30629bec1ace
Fred Emmott <fe@fb.com>
2020-07-10 15:42:46 -0700

    Don't install python with apt in .travis-deploy.sh
    - it's just for AWS EB
    - AWS EB installer will install python itself
    - AWS EB installer is not installing some parts because they're already
    on the system
    
    If this doesn't fix it, I'll try adding:
    
    ```
    cd $(pyenv root)
    git fetch --tags
    git checkout v1.2.19
    ```

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -34,11 +34,6 @@ cp .deploy/prod.Dockerfile "$REPO_OUT/Dockerfile"
 )
 
 echo "** Installing ElasticBeanstalk CLI..."
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update -y
-sudo apt-get install -y python3 python3-pip python3-setuptools
-pyenv install --list
-export PYTHONPATH="$(mktemp -d)"
 git clone --depth 10 https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer
 export PATH="${PYTHONPATH}/bin:${PATH}"
