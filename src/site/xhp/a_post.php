c1b56e9f7b0ded8aae01856610b41926f2f76035
jjergus <jjergus@fb.com>
2020-08-04 12:22:27 -0700

    fix a_post getID() call

--- src/site/xhp/a_post.php
+++ src/site/xhp/a_post.php
@@ -16,29 +16,23 @@ use type Facebook\XHP\HTML\{XHPHTMLHelpers, a, form};
 
 
 final xhp class a_post extends x\element {
-  use XHPHTMLHelpers;
-
   attribute string href @required;
   attribute string target;
 
   protected async function renderAsync(): Awaitable<x\node> {
-    $id = $this->getID();
-
     $anchor = <a>{$this->getChildren()}</a>;
-    $form = (
+    $form =
       <form
-        id={$id}
         method="post"
         action={$this->:href}
         target={$this->:target}
         class="postLink">
         {$anchor}
-      </form>
-    );
+      </form>;
 
     $anchor->setAttribute(
       'onclick',
-      'document.getElementById("'.$id.'").submit(); return false;',
+      'document.getElementById("'.$form->getID().'").submit(); return false;',
     );
     $anchor->setAttribute('href', '#');
 
