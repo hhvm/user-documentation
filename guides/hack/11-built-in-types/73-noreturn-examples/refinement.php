83393721912e7329c59415b9472ecfb6550b2456
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-08-30 20:47:32 +0200

    Leave it up to me to commit a failing test

--- guides/hack/11-built-in-types/73-noreturn-examples/refinement.php
+++ guides/hack/11-built-in-types/73-noreturn-examples/refinement.php
@@ -4,9 +4,9 @@ namespace Hack\UserDocumentation\Types\Noreturn\Examples\Refinement;
 
 <<__EntryPoint>>
 async function main_async(): Awaitable<void> {
-  $nullable_int = '' ? 0 : null;
+  $nullable_int = '_' ? 0 : null;
   if (!($nullable_int is nonnull)) {
-    invariant_violation('$arraykey must not be a string');
+    invariant_violation('$nullable_int must not be null');
   }
   // If we didn't fall into the if above, $nullable_int must be an int.
   takes_int($nullable_int);
