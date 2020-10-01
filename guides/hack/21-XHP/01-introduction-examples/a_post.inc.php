e4fbb6c6099149725e6ea900cc55f76db2ca9494
jjergus <jjergus@fb.com>
2020-10-01 15:40:06 -0700

    un-linkify a_post.inc.php

--- guides/hack/21-XHP/01-introduction-examples/a_post.inc.php
+++ guides/hack/21-XHP/01-introduction-examples/a_post.inc.php
@@ -1,1 +1,39 @@
-../../../../src/site/xhp/a_post.php
+<?hh
+
+namespace HHVM\UserDocumentation\Guides\Hack\XHP\Introduction\APost;
+
+use namespace Facebook\XHP\Core as x;
+use type Facebook\XHP\HTML\{XHPHTMLHelpers, a, form};
+
+
+final xhp class a_post extends x\element {
+  use XHPHTMLHelpers;
+
+  attribute string href @required;
+  attribute string target;
+
+  <<__Override>>
+  protected async function renderAsync(): Awaitable<x\node> {
+    $id = $this->getID();
+
+    $anchor = <a>{$this->getChildren()}</a>;
+    $form = (
+      <form
+        id={$id}
+        method="post"
+        action={$this->:href}
+        target={$this->:target}
+        class="postLink">
+        {$anchor}
+      </form>
+    );
+
+    $anchor->setAttribute(
+      'onclick',
+      'document.getElementById("'.$id.'").submit(); return false;',
+    );
+    $anchor->setAttribute('href', '#');
+
+    return $form;
+  }
+}
