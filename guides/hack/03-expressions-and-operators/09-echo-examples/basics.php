3814f4fb7a7423d60d5fa888968dab62283d6666
Prithviraj Billa <prithviraj.m.billa@gmail.com>
2020-11-02 11:57:34 -0500

    Fixes comment in echo examples

--- guides/hack/03-expressions-and-operators/09-echo-examples/basics.php
+++ guides/hack/03-expressions-and-operators/09-echo-examples/basics.php
@@ -6,7 +6,7 @@ namespace Hack\UserDocumentation\ExpAndOps\Echo\Examples\Basics;
 function main(): void {
   $v1 = true;
   $v2 = 123.45;
-  echo '>>'.$v1.'|'.$v2."<<\n"; // outputs ">>1|123<<"
+  echo '>>'.$v1.'|'.$v2."<<\n"; // outputs ">>1|123.45<<"
 
   $v3 = "abc{$v2}xyz";
   echo "$v3\n";
