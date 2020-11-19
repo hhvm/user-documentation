94f9877e0d4fa7ac8c7fd473dfcf36a66f763b4d
jjergus <jjergus@fb.com>
2020-11-19 13:44:31 -0800

    fix async mysql .skipif

--- api-examples/__includes/async_mysql_require_env.inc.php
+++ api-examples/__includes/async_mysql_require_env.inc.php
@@ -1,17 +1,16 @@
-<?hh // partial
+<?hh
 
 namespace Hack\UserDocumentation\API\Examples\AsyncMysql;
 
-use \Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
+use Hack\UserDocumentation\API\Examples\AsyncMysql\ConnectionInfo as CI;
 
-<<__EntryPoint>>
-function async_mysql_require_env_inc_main(): void {
+async function skipif_async(): Awaitable<void> {
   require __DIR__."/async_mysql_connect.inc.php";
 
-  if (!\extension_loaded('mysql') || !\function_exists('mysqli_connect')) {
-    die('Skip');
-  }
-  if (!mysqli_connect(CI::$host, CI::$user, CI::$passwd, CI::$db, CI::$port)) {
+  $pool = new \AsyncMysqlConnectionPool(darray[]);
+  try {
+    await $pool->connect(CI::$host, CI::$port, CI::$db, CI::$user, CI::$passwd);
+  } catch (\AsyncMysqlConnectException $_) {
     die('Skip');
   }
 }
