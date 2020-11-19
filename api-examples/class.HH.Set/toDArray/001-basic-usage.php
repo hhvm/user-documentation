19e0af7bb99799b8df79802445e44afedfdd8751
jjergus <jjergus@fb.com>
2020-11-19 14:06:53 -0800

    manually fix strict Hack errors

--- api-examples/class.HH.Set/toDArray/001-basic-usage.php
+++ api-examples/class.HH.Set/toDArray/001-basic-usage.php
@@ -3,7 +3,7 @@
 namespace Hack\UserDocumentation\API\Examples\Set\ToDArray;
 
 <<__EntryPoint>>
-function run() {
+function run(): void {
   $s = Set {'red', 'green', 'blue', 'yellow'};
 
   $array = $s->toDArray();
