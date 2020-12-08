df70ff0ce1a6b1b68bfbd7fbe5d0eb23566f06f0
jjergus <jjergus@fb.com>
2020-12-08 13:23:10 -0800

    target HHVM 4.87

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<d76ace8ab546853ccd8cf21c17c2c5ba>>
+ * @generated SignedSource<<7cae69a225eaf2ced9176c3381239012>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.86.1',
+  APIProduct::HACK => 'HHVM-4.87.0',
   APIProduct::HSL => 'v4.41.0',
-  APIProduct::HSL_EXPERIMENTAL => 'v4.66.0',
+  APIProduct::HSL_EXPERIMENTAL => 'v4.76.0',
 ];
