ad161653e4857c8fd477f87645657412eaaad560
Fred Emmott <fe@fb.com>
2021-11-02 16:12:03 -0500

    Update to 4.134.0

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<691e439516cbe6e76f7c1b3e52625faa>>
+ * @generated SignedSource<<f8539427089ddebb0e1f1a56b0bb202b>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.130.0',
+  APIProduct::HACK => 'HHVM-4.134.0',
   APIProduct::HSL => 'v4.108.1',
   APIProduct::HSL_EXPERIMENTAL => 'v4.108.0',
 ];
