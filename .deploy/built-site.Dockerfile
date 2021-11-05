051171c772f0f0588d25aef3788adaf75120cf6d
Fred Emmott <fe@fb.com>
2021-11-05 16:46:02 -0500

    make apt package installs part of docker image building

--- .deploy/built-site.Dockerfile
+++ .deploy/built-site.Dockerfile
@@ -1,7 +1,6 @@
 # Creates a docker image with a built copy of the site. Not repo-auth.
 # Useful as a scratch/testing area.
 FROM hhvm/hhvm:4.134-latest
-ARG DOCKER_BUILD_ENV=prod
 ENV TZ UTC
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US:en
@@ -10,4 +9,4 @@ ENV LC_ALL en_US.UTF-8
 RUN rm -rf /var/www
 ADD . /var/www
 
-RUN touch /docker_build && cd /var/www && .deploy/init.sh
+RUN cd /var/www && touch /.docker_build && .deploy/init.sh
--- .deploy/init.sh
+++ .deploy/init.sh
@@ -2,35 +2,17 @@
 
 set -ex
 
-if [ ! -e /docker_build ]; then
+if ! [ -e /.docker_build ]; then
   echo "This script should only be ran from a Dockerfile"
   exit 1
 fi
 
-export DEBIAN_FRONTEND=noninteractive
-
-echo "** Installing apt dependencies"
-# This is done by the dockerfile, but the intermediate issue can be cached, so do
-# it again here.
-apt-get clean
-apt-get update -y
-
-# Some environments (e.g. VSCode containers) will copy the exterior locale
-# settings, which can break things if the current locale isn't usable in the
-# container; using the `C` locale makes sure that the `locales` package
-# post-install succeeds.
-LC_ALL=C apt-get install -y ruby php-cli zip unzip locales
-
-echo "** Updating locales"
-locale-gen en_US.UTF-8
-
-echo "** Installing composer"
-mkdir /opt/composer
-wget -qO /dev/stdout https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-if [ ! -x /usr/local/bin/composer ]; then
-  echo "Failed to install composer"
+if ! [ -x .deploy/system-init.sh ]; then
+  echo "Run from the root directory of the source tree."
   exit 1
 fi
+
+.deploy/system-init.sh
 
 echo "** Installing Hack dependencies"
 composer install
