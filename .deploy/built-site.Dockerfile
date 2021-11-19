1e44f0d165ac187485c86c012cebda724418c6c8
Yang, Bo <atry@fb.com>
2021-11-19 23:32:09 +0000

    Update Hack to 4.136.0

--- .deploy/built-site.Dockerfile
+++ .deploy/built-site.Dockerfile
@@ -1,6 +1,6 @@
 # Creates a docker image with a built copy of the site. Not repo-auth.
 # Useful as a scratch/testing area.
-FROM hhvm/hhvm:4.134-latest
+FROM hhvm/hhvm:4.136-latest
 ENV TZ UTC
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US:en
--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<f8539427089ddebb0e1f1a56b0bb202b>>
+ * @generated SignedSource<<b0b060b8b4be1e31ca07aec5c470059a>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.134.0',
+  APIProduct::HACK => 'HHVM-4.136.0',
   APIProduct::HSL => 'v4.108.1',
   APIProduct::HSL_EXPERIMENTAL => 'v4.108.0',
 ];
