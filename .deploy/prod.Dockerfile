556f539a5b1b425d73eeb6697913cad659e25d33
Fred Emmott <fe@fb.com>
2021-12-20 13:54:31 -0600

    Update versions

--- .deploy/prod.Dockerfile
+++ .deploy/prod.Dockerfile
@@ -1,4 +1,4 @@
-FROM hhvm/hhvm-proxygen:4.136-latest
+FROM hhvm/hhvm-proxygen:4.140-latest
 
 ADD hhvm.prod.ini /etc/hhvm/site.ini
 ADD hhvm.hhbc /var/www/hhvm.hhbc
--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<b0b060b8b4be1e31ca07aec5c470059a>>
+ * @generated SignedSource<<a61bc399da8e8fca46397039e329571b>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.136.0',
+  APIProduct::HACK => 'HHVM-4.140.1',
   APIProduct::HSL => 'v4.108.1',
   APIProduct::HSL_EXPERIMENTAL => 'v4.108.0',
 ];
