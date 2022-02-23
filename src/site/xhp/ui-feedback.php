4debe32827754d23500064cc56eb2f8ba61f4d1d
Andrew DiMola <andrewdimola@gmail.com>
2022-02-23 21:20:09 +0000

    Rename variables / Write better code

--- src/site/xhp/ui-feedback.php
+++ src/site/xhp/ui-feedback.php
@@ -12,7 +12,7 @@
 namespace HHVM\UserDocumentation\ui;
 
 use namespace Facebook\XHP\Core as x;
-use type Facebook\XHP\HTML\{div, i, script};
+use type Facebook\XHP\HTML\{a, div, i, script};
 use type HHVM\UserDocumentation\{
   github_issue_link,
 };
@@ -22,71 +22,61 @@ final xhp class feedback extends x\element {
   <<__Override>>
   protected async function renderAsync(): Awaitable<x\node> {
     $thumbs_up =
-      <i class="fa fa-solid fa-thumbs-up fa-lg"></i>;
+      <a id="like-feedback" href="#"><i class="fa fa-solid fa-thumbs-up fa-lg"></i></a>;
 
     $thumbs_down =
-      <i class="fa fa-solid fa-thumbs-down fa-lg"></i>;
+      <a id="dislike-feedback" href="#"><i class="fa fa-solid fa-thumbs-down fa-lg"></i></a>;
 
-    $feedback =
-      <div id="form-basic">Was This Page Useful?
+    $feedback_message =
+      <div id="feedback-prompt-message">Was This Page Useful?
         {$thumbs_up}
         {$thumbs_down}
       </div>;
 
-    $good_feedback =
-      <div id="form-yes">Thank You!</div>;
+    $good_feedback_message =
+      <div id="like-message">Thank You!</div>;
 
-    $bad_feedback =
-      <div id="form-no">Thank You. Please <github_issue_link issueTitle={$this->getGithubIssueTitle()} issueBody={$this->getGithubIssueBody()}>File an Issue</github_issue_link> to help future readers!</div>;
+    $bad_feedback_message =
+      <div id="dislike-message">Thank You. If you have feedback, you can <github_issue_link issueTitle={$this->getGithubIssueTitle()} issueBody={$this->getGithubIssueBody()}>File an Issue</github_issue_link> to help future readers!</div>;
 
     $container = (
-      <div class="feedbackForm">
-        {$feedback}
-        {$good_feedback}
-        {$bad_feedback}
+      <div id="feedback-container" class="pageFeedback">
+        {$feedback_message}
+        {$good_feedback_message}
+        {$bad_feedback_message}
       </div>
     );
 
     $container->appendChild(varray[
-      $this->toggleGoodFeedbackScript($thumbs_up, $container),
-      $this->toggleBadFeedbackScript($thumbs_down, $container),
+      $this->toggleFeedbackScript(),
     ]);
 
     return $container;
   }
 
-  private function toggleGoodFeedbackScript(i $button, div $container): script {
-    $button_id = \json_encode($button->getID());
-    $container_id = \json_encode($container->getID());
+  private function toggleFeedbackScript(): script {
     return (
       <script language="javascript">
-        ga('send', 'event', 'Feedback', 'Useful', 'Guides Page', 1);
-        var toggleButton = document.getElementById({$button_id});
-        toggleButton.addEventListener(
+        var likeIcon = document.getElementById("like-feedback");
+        var dislikeIcon = document.getElementById("dislike-feedback");
+        likeIcon.addEventListener(
         'click',
-        function() {"{"}
-        var toggleContainer = document.getElementById({$container_id});
-        toggleContainer.classList.toggle('feedbackPromptOff');
-        toggleContainer.classList.toggle('goodFeedbackOn');
+        function(e) {"{"}
+        e.preventDefault();
+        var feedbackContainer = document.getElementById("feedback-container");
+        feedbackContainer.classList.toggle('feedbackPromptOff');
+        feedbackContainer.classList.toggle('likeFeedbackOn');
+        ga('send', 'event', 'Feedback', 'Useful', 'Guides Page', 1);
         {"}"}
         );
-      </script>
-    );
-  }
-
-  private function toggleBadFeedbackScript(i $button, div $container): script {
-    $button_id = \json_encode($button->getID());
-    $container_id = \json_encode($container->getID());
-    return (
-      <script language="javascript">
-        ga('send', 'event', 'Feedback', 'Not Useful', 'Guides Page', 0);
-        var toggleButton = document.getElementById({$button_id});
-        toggleButton.addEventListener(
+        dislikeIcon.addEventListener(
         'click',
-        function() {"{"}
-        var toggleContainer = document.getElementById({$container_id});
-        toggleContainer.classList.toggle('feedbackPromptOff');
-        toggleContainer.classList.toggle('badFeedbackOn');
+        function(e) {"{"}
+        e.preventDefault();
+        var feedbackContainer = document.getElementById("feedback-container");
+        feedbackContainer.classList.toggle('feedbackPromptOff');
+        feedbackContainer.classList.toggle('dislikeFeedbackOn');
+        ga('send', 'event', 'Feedback', 'Not Useful', 'Guides Page', 0);
         {"}"}
         );
       </script>
