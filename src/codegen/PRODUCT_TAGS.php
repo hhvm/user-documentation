85a15b030e4a0982783ac7fb8f7ed85ed2d42f0c
Fred Emmott <fe@fb.com>
2022-03-25 14:04:17 -0500

    Update to HHVM 4.154
    - trait changes were just text changes to the error text
    - AsyncMysqlClient::adoptConnection was removed

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<2262ecbcbe027dce4d28b805ca3c49c8>>
+ * @generated SignedSource<<cb1185ac93592547f05c7f139b42211d>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.147.0',
+  APIProduct::HACK => 'HHVM-4.154.0',
   APIProduct::HSL => 'v4.108.1',
   APIProduct::HSL_EXPERIMENTAL => 'v4.108.0',
 ];
