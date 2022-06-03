f0f6c9fa8329e76d5a7cf98f2d4b53b99763be97
Wilfred Hughes <wilfred@fb.com>
2022-06-02 17:34:19 -0700

    Revert "Revert "Update HHVM to 4.160 (#1220)" to test deploy"
    This reverts commit 1071f1b2db071de0fb81fe1183ea58fd02fccbbd.

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<cb1185ac93592547f05c7f139b42211d>>
+ * @generated SignedSource<<3a5e67a90bcf26a3d8d406c0d568a52f>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.154.0',
+  APIProduct::HACK => 'HHVM-4.160.0',
   APIProduct::HSL => 'v4.108.1',
   APIProduct::HSL_EXPERIMENTAL => 'v4.108.0',
 ];
