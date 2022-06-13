8dfbed6a032d949adbb35e5bfcef5d9da5cc11da
Yang, Bo <atry@fb.com>
2022-06-13 13:21:17 -0700

    Revert "Limit the length of the environment name"
    This reverts commit afbf9ad5b31fc7908beb943927bcf26689fd71e7.

--- .deploy/terraform/staging.tf
+++ .deploy/terraform/staging.tf
@@ -24,7 +24,7 @@ resource "aws_elastic_beanstalk_application_version" "docs_staging" {
 
 resource "aws_elastic_beanstalk_environment" "docs_staging" {
   application = aws_elastic_beanstalk_application.docs.name
-  name = "hhvm-hack-docs-vpc-staging-${substr(uuid(), 0, 10)}"
+  name = "hhvm-hack-docs-vpc-staging-${uuid()}"
   cname_prefix = "hack-hhvm-docs-vpc-staging"
   template_name = aws_elastic_beanstalk_configuration_template.docs.name
   version_label = aws_elastic_beanstalk_application_version.docs_staging.id
