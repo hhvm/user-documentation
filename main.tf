d2d341bd5f7e4fb14a29dd4ab6e88b6593698709
Yang, Bo <atry@fb.com>
2022-06-14 16:27:40 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.17.0"
+    }
+  }
+  backend "s3" {
+    region = "us-west-2"
+    bucket = "hack-docs-terraform-state"
+    key = "hack-docs/tfstate"
+    dynamodb_table = "hack-docs-tfstate"
+  }
+}
