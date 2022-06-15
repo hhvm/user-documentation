d04cc99d0fe75e59203d6d31d63569c6e821c0e7
Yang, Bo <atry@fb.com>
2022-06-15 11:51:40 -0700

    Rename https port

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "ecs-fargate" {
   default_certificate_arn = "arn:aws:acm:us-west-2:223121549624:certificate/8f845b56-937f-49b8-adf4-64b69a3caf57"
 
   lb_https_ports = {
-    default = {
+    forward_https_to_http = {
       listener_port     = 443
       target_group_port = 80
     }
