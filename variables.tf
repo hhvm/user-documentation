2b086671f52a716b56fd019b607762a4a685c662
Yang, Bo <atry@fb.com>
2022-06-15 16:42:09 -0700

    Degrade to an old docs site versio

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "container_image" {
   type        = string
-  default     = "hhvm/user-documentation:HHVM-4.160-2022-06-14-d0f8227"
+  default     = "hhvm/user-documentation:HHVM-4.154-2022-04-27-164b4ea"
 }
