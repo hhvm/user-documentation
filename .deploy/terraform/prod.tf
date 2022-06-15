17722d87793e61a10252ccb01e449fad3b744f85
Yang, Bo <atry@fb.com>
2022-06-15 13:12:09 -0700

    Remove all resources

--- .deploy/terraform/prod.tf
+++ .deploy/terraform/prod.tf
@@ -8,27 +8,3 @@ data "template_file" "prod_appversion" {
     image_tag = var.prod_docker_image
   }
 }
-
-resource "aws_s3_bucket_object" "prod_appversion" {
-  bucket = "hack-docs-terraform-state"
-  key = "hack-docs/prod-${terraform.workspace}-versions/${var.prod_docker_image}.json"
-  content = data.template_file.prod_appversion.rendered
-}
-
-resource "aws_elastic_beanstalk_application_version" "docs_prod" {
-  application = aws_elastic_beanstalk_application.docs.name
-  name = "tf-prod-${terraform.workspace}-${var.prod_docker_image}"
-  bucket = aws_s3_bucket_object.prod_appversion.bucket
-  key = aws_s3_bucket_object.prod_appversion.id
-}
-
-resource "aws_elastic_beanstalk_environment" "docs_prod" {
-  application = aws_elastic_beanstalk_application.docs.name
-  name = "hhvm-hack-docs-vpc-prod-${terraform.workspace}"
-  cname_prefix = "hack-hhvm-docs-vpc-prod-${terraform.workspace}"
-  template_name = aws_elastic_beanstalk_configuration_template.docs.name
-  version_label = aws_elastic_beanstalk_application_version.docs_prod.id
-  tags = {
-    DockerImage = var.prod_docker_image
-  }
-}
--- .deploy/terraform/staging.tf
+++ .deploy/terraform/staging.tf
@@ -8,27 +8,3 @@ data "template_file" "staging_appversion" {
     image_tag = var.staging_docker_image
   }
 }
-
-resource "aws_s3_bucket_object" "staging_appversion" {
-  bucket = "hack-docs-terraform-state"
-  key = "hack-docs/staging-${terraform.workspace}-versions/${var.staging_docker_image}.json"
-  content = data.template_file.staging_appversion.rendered
-}
-
-resource "aws_elastic_beanstalk_application_version" "docs_staging" {
-  application = aws_elastic_beanstalk_application.docs.name
-  name = "tf-staging-${terraform.workspace}-${var.staging_docker_image}"
-  bucket = aws_s3_bucket_object.staging_appversion.bucket
-  key = aws_s3_bucket_object.staging_appversion.id
-}
-
-resource "aws_elastic_beanstalk_environment" "docs_staging" {
-  application = aws_elastic_beanstalk_application.docs.name
-  name = "hhvm-hack-docs-vpc-staging-${terraform.workspace}"
-  cname_prefix = "hack-hhvm-docs-vpc-staging-${terraform.workspace}"
-  template_name = aws_elastic_beanstalk_configuration_template.docs.name
-  version_label = aws_elastic_beanstalk_application_version.docs_staging.id
-  tags = {
-    DockerImage = var.staging_docker_image
-  }
-}
