b1d167b5ffdb393679d40c7833492979cef7a015
Yang, Bo <atry@fb.com>
2022-06-17 15:02:21 -0700

    Increase CPU and memory

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ module "ecs-fargate" {
   private_subnets_ids          = module.networking.private_subnets_ids
   container_name               = "docs-${terraform.workspace}"
   container_image              = var.container_image
-  container_cpu                = 256
-  container_memory             = 2048
+  container_cpu                = 2 * 1024
+  container_memory             = 16 * 1024
   container_memory_reservation = null
 
   enable_s3_logs = false
