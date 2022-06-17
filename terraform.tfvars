8c20a5fd3c4ec32c480d8557567098f1692283ff
Yang, Bo <atry@fb.com>
2022-06-16 17:24:48 -0700

    Move the container_image value to a separate tfvars file

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+container_image = "hhvm/user-documentation:HHVM-4.154-2022-04-27-164b4ea"
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "container_image" {
   type        = string
-  default     = "hhvm/user-documentation:HHVM-4.154-2022-04-27-164b4ea"
+  description = "The Docker image to deploy. For example: hhvm/user-documentation:HHVM-4.154-2022-04-27-164b4ea"
 }
