02af6e20b2ce770f0a78b9dfcada9924da0c8ee8
Yang, Bo <atry@fb.com>
2022-06-16 21:10:50 -0700

    Set required version

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = "1.2.3"
+}
+
 module "networking" {
   source                                      = "cn-terraform/networking/aws"
   name_prefix                                 = "docs-${terraform.workspace}"
