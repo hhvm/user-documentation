0fc9ef5a244e4eeff596806af892cec1c5d676f3
Andrew <andrewdimola@gmail.com>
2022-07-14 19:25:59 +0000

    Invoke upgrade script

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<3a5e67a90bcf26a3d8d406c0d568a52f>>
+ * @generated SignedSource<<27c0c9a0df9e3256d320e0a0877d8964>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.160.0',
+  APIProduct::HACK => 'HHVM-4.164.0',
   APIProduct::HSL => 'v4.108.1',
   APIProduct::HSL_EXPERIMENTAL => 'v4.108.0',
 ];
