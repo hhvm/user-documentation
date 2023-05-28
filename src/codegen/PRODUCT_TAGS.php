f08aaebee4d42f54654d2aa765c24906b7e5d9a9
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2023-05-29 01:40:34 +0200

    Upgrade to hhvm 4.168 LTS

--- src/codegen/PRODUCT_TAGS.php
+++ src/codegen/PRODUCT_TAGS.php
@@ -2,12 +2,12 @@
 /**
  * This file is generated. Do not modify it manually!
  *
- * @generated SignedSource<<27c0c9a0df9e3256d320e0a0877d8964>>
+ * @generated SignedSource<<2f46ffed5a0b2e32ea3b7b1780b59271>>
  */
 namespace HHVM\UserDocumentation;
 
 const dict<APIProduct, string> PRODUCT_TAGS = dict[
-  APIProduct::HACK => 'HHVM-4.164.0',
+  APIProduct::HACK => 'HHVM-4.168.2',
   APIProduct::HSL => 'v4.108.1',
   APIProduct::HSL_EXPERIMENTAL => 'v4.108.0',
 ];
