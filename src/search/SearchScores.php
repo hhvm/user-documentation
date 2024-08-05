b0f78185f1076591d42d13c129724f643cebab23
Dwayne Reeves <dwaynelreeves@gmail.com>
2024-08-05 20:36:25 +0000

    Undo Format Change

--- src/search/SearchScores.php
+++ src/search/SearchScores.php
@@ -25,7 +25,6 @@ abstract final class SearchScores {
 
   // e.g. search for 'map set', find HH\Map::set
   const float IN_ORDER_WORD_SPLIT_MULTIPLIER = 0.9;
-
   // e.g. search for 'map set', find HH\Set::map
   const float OUT_OF_ORDER_WORD_SPLIT_MULTIPLIER = 0.5;
 
